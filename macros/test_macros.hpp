cde479cca4472bf0c772433abc5c0c457216058b
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2012-07-16 08:04:35 +0000

    Finished moving files to the Cpp directory
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/macros@1314 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ macros/test_macros.hpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   test_macros.hpp
+ * Author: leroux
+ *
+ * Created on August 22, 2011, 3:44 PM
+ */
+
+#ifndef TEST_MACROS_HPP
+#define	TEST_MACROS_HPP
+#include <iostream>
+
+#define COUT(val) std::cout << "in file " << __FILE__ << ":" << __LINE__ << " " << #val << " = " << val << std::endl;
+
+#endif	/* TEST_MACROS_HPP */
+
