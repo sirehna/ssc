79db199ac659c39e3011e5d81975b5cbffe8b9e1
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2012-08-03 12:59:21 +0000

    UNSTABLE Unified random data generators
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/random_data_generator@1330 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- random_data_generator/src/DataGenerator.cpp
+++ random_data_generator/src/DataGenerator.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 
-//#define MAX_RAND_INT RAND_MAX
 #define MAX_RAND_INT 2147483647
 
 #define MIN(a,b) (a<b) ? a : b;
@@ -25,13 +24,6 @@ double DataGenerator::random_double() const
     return random_double(-1e6,1e6);
 }
 
-double DataGenerator::random_double_greater_than(const double& x0) const
-{
-    const double max_bound = (x0 < 1e6-5 ? 1e6 : 2*x0);
-    return random_double(x0,max_bound);
-    
-}
-
 int DataGenerator::random_int() const
 {
     return random_int(-1e6,1e6);
@@ -40,12 +32,6 @@ int DataGenerator::random_int() const
 size_t DataGenerator::random_size_t() const
 {
     return random_size_t(0,1e4);
-}
-
-size_t DataGenerator::random_size_t_greater_than(const size_t& i0) const
-{
-    const size_t max_bound = (i0 < 1e4-5 ? 1e4 : 2*i0);
-    return random_size_t(i0,max_bound);
 }
 
 float DataGenerator::random_float() const
@@ -62,8 +48,6 @@ short DataGenerator::random_short() const
 {
     return random_short(-1e2,1e2);
 }
-
-
 
 double DataGenerator::random_double(const double& a, const double& b) const
 {
@@ -133,28 +117,6 @@ const std::string DataGenerator::random_string() const
     return random_string(GET_A_MIN_STRING_LENGTH, GET_A_MAX_STRING_LENGTH);
 }
 
-const std::vector<bool>        DataGenerator::vector_of_random_booleans(const size_t& n) const
-{
-    std::vector<bool> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_bool());
-    }
-    return ret;
-}
-
-const std::vector<int>         DataGenerator::vector_of_random_integers(const size_t& n, const int& a, const int& b) const
-{
-    std::vector<int> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_int(a, b));
-    }
-    return ret;
-}
-
 const std::vector<size_t>      DataGenerator::vector_of_random_size_ts(const size_t& n, const size_t& a, const size_t& b) const
 {
     std::vector<size_t> ret;
@@ -162,39 +124,6 @@ const std::vector<size_t>      DataGenerator::vector_of_random_size_ts(const siz
     for (size_t i = 0 ; i < n ; ++i)
     {
         ret.push_back(random_size_t(a, b));
-    }
-    return ret;
-}
-
-const std::vector<short>       DataGenerator::vector_of_random_shorts(const size_t& n, const short& a, const short& b) const
-{
-    std::vector<short> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_short(a,b));
-    }
-    return ret;
-}
-
-const std::vector<long>        DataGenerator::vector_of_random_longs(const size_t& n, const long& a, const long& b) const
-{
-    std::vector<long> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_long(a, b));
-    }
-    return ret;
-}
-
-const std::vector<float>       DataGenerator::vector_of_random_floats(const size_t& n, const float& a, const float& b) const
-{
-    std::vector<float> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_float(a, b));
     }
     return ret;
 }
@@ -210,44 +139,9 @@ const std::vector<double>      DataGenerator::vector_of_random_doubles(const siz
     return ret;
 }
 
-const std::vector<double>      DataGenerator::vector_of_random_doubles(const size_t& n) const
-{
-    const double min_bound = random_double();
-    return vector_of_random_doubles(n, min_bound, random_double_greater_than(min_bound));
-}
-
-const std::vector<char>        DataGenerator::vector_of_random_chars(const size_t& n) const
-{
-    std::vector<char> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_char());
-    }
-    return ret;
-}
-
-const std::vector<std::string> DataGenerator::vector_of_random_strings(const size_t& n) const
-{
-    std::vector<std::string> ret;
-    ret.reserve(n);
-    for (size_t i = 0 ; i < n ; ++i)
-    {
-        ret.push_back(random_string());
-    }
-    return ret;
-}
-
 int DataGenerator::get_random_number() const
 {
 	return static_cast<int>(sir_rand_u01()*MAX_RAND_INT);
-    //return rand();
-}
-
-
-const std::pair<int,int> DataGenerator::random_pair_of_int() const
-{
-    return std::pair<int,int>(random_int(),random_int());
 }
 
 template <> double TypedScalarDataGenerator<double>::get() const
@@ -277,7 +171,6 @@ template <> std::string TypedScalarDataGenerator<std::string>::get() const
 {
     return random_string();
 }
-
 
 template <> std::vector<double> TypedVectorDataGenerator<double>::get() const
 {
--- random_data_generator/unit_tests/src/random_data_generator_tests.cpp
+++ random_data_generator/unit_tests/src/random_data_generator_tests.cpp
@@ -16,10 +16,10 @@ TEST_F(DataGeneratorTests, should_have_a_random_data_generator_for_doubles)
     for (size_t i =0 ; i < nb_of_trials ; ++i)
     {
         S.clear();
-        const size_t nb_of_values_to_generate = a.random_size_t(1,100);
+        const size_t nb_of_values_to_generate = a.random<size_t>().between(1,100);
         for (size_t j = 0 ; j < nb_of_values_to_generate ; ++j)
         {
-            S.insert(a.random<double>()());
+            S.insert(a.random<double>());
         }
         ASSERT_EQ(nb_of_values_to_generate, S.size());
     }
@@ -32,10 +32,10 @@ TEST_F(DataGeneratorTests, should_have_a_random_data_generator_for_vector_of_dou
     for (size_t i =0 ; i < nb_of_trials ; ++i)
     {
         S.clear();
-        const size_t nb_of_values_to_generate = a.random_size_t(1,5);
+        const size_t nb_of_values_to_generate = a.random<size_t>().between(1,5);
         for (size_t j = 0 ; j < nb_of_values_to_generate ; ++j)
         {
-            const std::vector<double> v = a.random_vector_of<double>().of_size(3)();
+            const std::vector<double> v = a.random_vector_of<double>().of_size(3);
             ASSERT_FALSE(v.empty());
             S.insert(v);
         }
@@ -50,7 +50,7 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_doubles_greater_than_a_val
     std::vector<double> v(nb_of_trials);
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double lower_bound = a.random<double>()();
+        const double lower_bound = a.random<double>();
         ASSERT_GE(a.random<double>().greater_than(lower_bound)(), lower_bound);
     }
 }
@@ -61,7 +61,7 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_doubles_no_greater_than_a_
     std::vector<double> v(nb_of_trials);
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double upper_bound = a.random<double>()();
+        const double upper_bound = a.random<double>();
         ASSERT_LE(a.random<double>().no().greater_than(upper_bound)(), upper_bound);
     }
 }
@@ -72,9 +72,9 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_a_vector_of_doubles_greate
     std::vector<double> v(nb_of_trials);
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double lower_bound = a.random<double>()();
-        const size_t n = a.random<size_t>().no().greater_than(20)();
-        const std::vector<double> v = a.random_vector_of<double>().of_size(n).greater_than(lower_bound)();
+        const double lower_bound = a.random<double>();
+        const size_t n = a.random<size_t>().no().greater_than(20);
+        const std::vector<double> v = a.random_vector_of<double>().of_size(n).greater_than(lower_bound);
         for (std::vector<double>::const_iterator it = v.begin() ; it != v.end() ; ++it)
         {
             ASSERT_GE(*it, lower_bound);
@@ -88,9 +88,9 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_a_vector_of_doubles_no_gre
     std::vector<double> v(nb_of_trials);
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double upper_bound = a.random<double>()();
-        const size_t n = a.random<size_t>().no().greater_than(20)();
-        const std::vector<double> v = a.random_vector_of<double>().of_size(n).no().greater_than(upper_bound)();
+        const double upper_bound = a.random<double>();
+        const size_t n = a.random<size_t>().no().greater_than(20);
+        const std::vector<double> v = a.random_vector_of<double>().of_size(n).no().greater_than(upper_bound);
         for (std::vector<double>::const_iterator it = v.begin() ; it != v.end() ; ++it)
         {
             ASSERT_LE(*it, upper_bound);
@@ -103,8 +103,8 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_doubles_between_two_values
     const size_t nb_of_trials = 100;
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double lower_bound = a.random<double>()();
-        const double upper_bound = a.random<double>().greater_than(lower_bound)();
+        const double lower_bound = a.random<double>();
+        const double upper_bound = a.random<double>().greater_than(lower_bound);
         ASSERT_LE(a.random<double>().between(lower_bound,upper_bound)(), upper_bound);
         ASSERT_GE(a.random<double>().between(lower_bound,upper_bound)(), lower_bound);
     }
@@ -116,8 +116,8 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_doubles_outside_an_interva
     std::set<double> generated_numbers;
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double number1 = a.random<double>().outside(2.1,2.9).but().between(2,3)();
-        const double number2 = a.random<double>().between(2,3).but().outside(2.1,2.9)();
+        const double number1 = a.random<double>().outside(2.1,2.9).but().between(2,3);
+        const double number2 = a.random<double>().between(2,3).but().outside(2.1,2.9);
         ASSERT_LT(number1, 3);
         ASSERT_GT(number1, 2);
         ASSERT_FALSE((number1>=2.1)&&(number1<=2.9));
@@ -135,8 +135,8 @@ TEST_F(DataGeneratorTests, should_return_zero_if_forbidden_interval_matches_allo
     const size_t nb_of_trials = 100;
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const double min_bound = a.random<double>()();
-        const double max_bound = a.random<double>().greater_than(min_bound)();
+        const double min_bound = a.random<double>();
+        const double max_bound = a.random<double>().greater_than(min_bound);
         ASSERT_EQ(a.random<double>().between(min_bound,max_bound).but().outside(min_bound,max_bound)(),0);
     }
 }
@@ -148,10 +148,10 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_size_ts_for_all_but_one_va
     const size_t nb_of_trials = 100;
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        const size_t lower_bound = a.random<size_t>().between(1,5)();
-        const size_t upper_bound = a.random<size_t>().between(1,5).greater_than(lower_bound)();
-        const size_t forbidden = a.random<size_t>().between(lower_bound, upper_bound)();
-        const size_t number = a.random<size_t>().between(lower_bound, upper_bound).but_not(forbidden)();
+        const size_t lower_bound = a.random<size_t>().between(1,5);
+        const size_t upper_bound = a.random<size_t>().between(1,5).greater_than(lower_bound);
+        const size_t forbidden = a.random<size_t>().between(lower_bound, upper_bound);
+        const size_t number = a.random<size_t>().between(lower_bound, upper_bound).but_not(forbidden);
         ASSERT_NE(number, forbidden);
     }
 }
@@ -162,7 +162,7 @@ TEST_F(DataGeneratorTests, should_be_able_to_generate_a_random_double)
     std::set<double> generated_numbers;
     for (size_t i = 0 ; i < nb_of_trials ; ++i)
     {
-        generated_numbers.insert(a.random<bool>()());
+        generated_numbers.insert(a.random<bool>());
     }
     ASSERT_EQ(generated_numbers.size(), 2);
 }
