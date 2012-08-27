f1303d8d0b1a029180483e18cb189ba78f679d66
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2012-08-27 10:10:56 +0000

    Separated YAML parser in two: now have to link dynamically to libyaml-cpp Compiles, links & executes on windows
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/text_file_reader@1374 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ text_file_reader/unit_tests/inc/TextFileReaderTest.hpp
@@ -0,0 +1,26 @@
+/*
+ * TextFileReaderTest.hpp
+ *
+ * \date 21 mars 2012, 15:18:20
+ *  \author cec
+ */
+
+#ifndef TEXTFILEREADERTEST_HPP_
+#define TEXTFILEREADERTEST_HPP_
+
+#include "gtest/gtest.h"
+#include "DataGenerator.hpp"
+
+
+class TextFileReaderTest : public ::testing::Test
+{
+    protected:
+        TextFileReaderTest();
+	    virtual ~TextFileReaderTest();
+        virtual void SetUp();
+        virtual void TearDown();
+		DataGenerator a;
+
+};
+
+#endif  /* TEXTFILEREADERTEST_HPP_ */
---
+++ text_file_reader/unit_tests/src/TextFileReaderTest.cpp
@@ -0,0 +1,80 @@
+/*
+ * TextFileReaderTest.cpp
+ *
+ * \date 21 mars 2012, 15:18:47
+ *  \author cec
+ */
+
+#include "TextFileReaderTest.hpp"
+#include "TextFileReader.hpp"
+
+#include "test_macros.hpp"
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+TextFileReaderTest::TextFileReaderTest() : a(DataGenerator(718293))
+{
+}
+
+TextFileReaderTest::~TextFileReaderTest()
+{
+
+}
+
+void TextFileReaderTest::SetUp()
+{
+
+}
+
+void TextFileReaderTest::TearDown()
+{
+}
+
+TEST_F(TextFileReaderTest, example)
+{
+//! [TextFileReaderTest example]
+	char filename1 [L_tmpnam];
+	char filename2 [L_tmpnam];
+	tmpnam (filename1);
+	tmpnam (filename2);
+	std::ofstream file1 (filename1);
+	const std::string contents_file1 = a.random<std::string>();
+	const std::string contents_file2 = a.random<std::string>();
+	file1 << contents_file1;
+	file1.close();
+	std::ofstream file2 (filename2);
+	file2 << contents_file2;
+	file2.close();
+	std::vector<std::string> filenames;
+	filenames.push_back(filename1);
+	filenames.push_back(filename2);
+	TextFileReader reader(filenames);
+	remove (filename1);
+	remove (filename2);
+//! [TextFileReaderTest example]
+//! [TextFileReaderTest expected output]
+	ASSERT_EQ(contents_file1+contents_file2, reader.get_contents());
+//! [TextFileReaderTest expected output]
+}
+
+TEST_F(TextFileReaderTest, should_represent_newlines_correctly)
+{
+	char filename [L_tmpnam];
+	tmpnam (filename);
+	std::ofstream file (filename);
+	const std::string contents_file = a.random<std::string>()()
+			                         + std::string("\n")
+	                                 + a.random<std::string>()()
+	                                 + std::string("\n")
+	                                 + a.random<std::string>()();
+	file << contents_file;
+	file.close();
+	std::vector<std::string> filenames;
+	filenames.push_back(filename);
+	TextFileReader reader(filenames);
+	remove (filename);
+	ASSERT_EQ(contents_file, reader.get_contents());
+}
+
