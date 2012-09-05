7a3cc94ff77295dcab16900089f120b3b2aa49d3
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2012-09-05 06:31:26 +0000

    Added a YamlParser module in Tools
    AllocatorYamlParser now derives from this YamlParser class which can be
    reused independantly.
    
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/yaml_parser@1447 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ yaml_parser/inc/YamlParser.hpp
@@ -0,0 +1,27 @@
+/*
+ * YamlParser.hpp
+ *
+ * \date 27 août 2012, 13:05:23
+ *  \author cec
+ */
+
+#ifndef YAMLPARSER_HPP_
+#define YAMLPARSER_HPP_
+
+#include "yaml.h"
+
+class YamlParser
+{
+	public:
+	virtual ~YamlParser() {}
+	YamlParser(const std::string& contents_to_parse);
+
+
+	protected:
+		YAML::Node& convert_stream_to_yaml_node(const std::string& input_data, YAML::Node& node) const;
+		YamlParser();
+		std::string contents;
+};
+
+
+#endif /* YAMLPARSER_HPP_ */
