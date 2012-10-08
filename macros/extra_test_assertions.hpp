794c14c8baa0f8ad6fb91d9300166eb2d5aaa44c
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2012-10-08 10:49:31 +0000

    Interpolation thrusters (for propeller/rudders) works on a single allocation
    - Refactored ShipOptimizationProblemTest (renamed some variables)
    - Allocator: added a method to check that nb of constraints in optimization problem matches nb of constraints in results
    - Allocator: get_results now checks that the nb of constraints match & throws if they don't (added this because of a crash
    in Allocator if the optimizer crashed (because result is then empty))
    - ShipOptimizationProblem: added check that nb of constraints in optimization problem matches nb of constraints in results
    (throw otherwise)
    - IpoptOptimizer: added a comment saying how to use Ipopt's second-derivative approximation (because 2nd derivative for
    splines doesn't necessarily produce what we want), but in fact it doesn't seem to be needed
    - OptimizationresultsTest: Corrected typos in naming of tests
    - InterpolatedThrusterConfiguration::has_point: added a hack because IPOPT doesn't respect the bounds on the state variables.
    Indeed, when defining a constant state (i.e. min bound = max bound), IPOPT can ask for the value of the cost for another value (usually
    not too far from the (fixed) state value). This meant that the InterpolationThruster was unable to find a suitable configuration for the
    (command,theta) pair
    - Wrote a test for InterpolatedThruster::hes_Fx() but then disabled it because, when using splines, cubic polynomials are not invariant
    (if we compute a spline for [0,1,2,3] and [0,1,8,27], we don't get x->x^3)
    - Added std::cerr to Exception.hpp but then commented it out: only there for debugging purposes
    - Added a header for ASSERT_NEAR
    - Made the Spline tests a bit nastier (more values & more repetitions)
    - Corrected error in yaml_data_generator: min_thruster_angle & max_thruster_angle were inverted
    
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/macros@1518 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ macros/extra_test_assertions.hpp
@@ -0,0 +1,14 @@
+/*
+ * extra_test_assertions.hpp
+ *
+ * \date 8 oct. 2012, 08:27:53
+ *  \author cec
+ */
+
+#ifndef EXTRA_TEST_ASSERTIONS_HPP_
+#define EXTRA_TEST_ASSERTIONS_HPP_
+
+#define ASSERT_SMALL_RELATIVE_ERROR(expected, actual, tol) ASSERT_NEAR(expected, actual, fabs(expected)*tol > tol ? fabs(expected)*tol:tol)
+
+
+#endif /* EXTRA_TEST_ASSERTIONS_HPP_ */
