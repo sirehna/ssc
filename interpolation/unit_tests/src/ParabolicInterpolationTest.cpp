4df9010d00c92b46a7a0fee42d328d384ba8f50c
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2013-02-20 10:02:04 +0000

    ParabolicInterpolation now appropriately throws when preconditions are not met (invalid size or xmax<xmin)
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/interpolation@1571 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- interpolation/unit_tests/src/ParabolicInterpolationTest.cpp
+++ interpolation/unit_tests/src/ParabolicInterpolationTest.cpp
@@ -24,6 +24,23 @@ void ParabolicInterpolationTest::TearDown()
 {
 }
 
+template <> ParabolicCoefficients TypedScalarDataGenerator<ParabolicCoefficients>::get() const
+{
+    ParabolicCoefficients ret(random<double>(),random<double>(),random<double>());
+    return ret;
+}
+
+template<> ParabolicCoefficients get_max_bound<ParabolicCoefficients>()
+{
+    return ParabolicCoefficients(0,0,0);
+}
+
+template<> ParabolicCoefficients get_min_bound<ParabolicCoefficients>()
+{
+    return ParabolicCoefficients(0,0,0);
+}
+
+
 TEST_F(ParabolicInterpolationTest, example)
 {
 //! [ParabolicInterpolationTest example]
@@ -47,5 +64,62 @@ TEST_F(ParabolicInterpolationTest, example)
 //! [ParabolicInterpolationTest expected output]
 }
 
+TEST_F(ParabolicInterpolationTest, derivative)
+{
+    std::vector<ParabolicCoefficients> coeffs;
+    coeffs.push_back(ParabolicCoefficients(12./5.,0,1./5.));
+    coeffs.push_back(ParabolicCoefficients(-6.,84./5.,-41./5.));
+    coeffs.push_back(ParabolicCoefficients(18./5.,-108./5.,151./5.));
+    ParabolicInterpolation pp(0, 3, coeffs);
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double X = a.random<double>().between(0,1);
+        pp.set_computed_value(X);
+        ASSERT_DOUBLE_EQ(2.*(12./5.)*X,pp.df());
+    }
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double X = a.random<double>().between(1,2);
+        pp.set_computed_value(X);
+        ASSERT_DOUBLE_EQ((84./5.)-2.*6.*X,pp.df());
+    }
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double X = a.random<double>().between(2,3);
+        pp.set_computed_value(X);
+        ASSERT_DOUBLE_EQ(-(108./5.)+2.*(18./5.)*X,pp.df());
+    }
+}
 
+TEST_F(ParabolicInterpolationTest, should_throw_if_coeffs_has_too_few_elements)
+{
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double xmin = a.random<double>();
+        const double xmax = a.random<double>().greater_than(xmin);
+        const size_t n = a.random<size_t>().no().greater_than(1);
+        ASSERT_THROW(ParabolicInterpolation(xmin, xmax, a.random_vector_of<ParabolicCoefficients>().of_size(n)), ParabolicInterpolationException);
+    }
+}
 
+TEST_F(ParabolicInterpolationTest, should_throw_if_xmin_greater_than_xmax)
+{
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double xmax = a.random<double>();
+        const double xmin = a.random<double>().greater_than(xmax);
+        const size_t n = a.random<size_t>().greater_than(1).but().no().greater_than(1000);
+        ASSERT_THROW(ParabolicInterpolation(xmin, xmax, a.random_vector_of<ParabolicCoefficients>().of_size(n)), ParabolicInterpolationException);
+    }
+}
+
+TEST_F(ParabolicInterpolationTest, should_not_throw_if_xmin_equals_xmax)
+{
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        const double xmin = a.random<double>();
+        const double xmax = xmin;
+        const size_t n = a.random<size_t>().greater_than(1).but().no().greater_than(1000);
+        ASSERT_NO_THROW(ParabolicInterpolation(xmin, xmax, a.random_vector_of<ParabolicCoefficients>().of_size(n)));
+    }
+}
