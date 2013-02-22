7927202d6c4758f6cd55546d57633791a6471e6e
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-02-22 14:28:07 +0000

    Ce commit ne concerne que le module "optimizer". On peut désormais borner les états d'un problème d'optimisation. - Création d'une fonction 'append' pour ajouter des StatePtr à une StateList existante (afin de pouvoir ajouter les états au fur et à mesure lors de la construction du problème d'otpimisation). - Afin d'utiliser un std::map de Parameter, nécessité de définir le constructeur par défaut Parameter::Parameter(). En pratique, pas forcément gênant : la valeur du Parameter est initialisée à zéro, ce qui correspond à l'effet probablement attendu. - Ajout d'un constructeur StateGetter::StateGetter(const StateList& list) pour pouvoir continuer à ajouter des StatePtr à partir d'une liste existante. - Ajout des méthodes OptimizationProblem::bound pour borner des états d'un problème d'optimisation. Au début, utilisation de std::unordered_map, mais c'est compliqué avec les std::tr1::shared_ptr donc on a choisi d'utiliser un std::map classique avec comme clef la sérialisation du noeud.
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@298 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/optimizer/src/OptimizationProblem.cpp
+++ ssc/optimizer/src/OptimizationProblem.cpp
@@ -8,39 +8,50 @@
 #include "OptimizationProblem.hpp"
 #include "Parameter.hpp"
 #include "Null.hpp"
+#include <map>
+#include "State.hpp"
+#include "Serialize.hpp"
 
-template <class T> class MinMax
+template <class T> class MinMaxList
 {
     public:
-        MinMax(const Parameter& min_, const T& val_, const Parameter& max_) : min(min_), max(max_), val(val_)
-        {
+        MinMaxList() : min(std::map<std::string,Parameter> ()),
+                       max(std::map<std::string,Parameter>()),
+                       val(std::vector<T>()){}
 
-        }
-        Parameter min;
-        Parameter max;
-        T val;
-    private:
-        MinMax();
-};
-
-class OptimizationProblem::OptimizationProblem_pimpl
-{
-    public:
-        OptimizationProblem_pimpl() : objective_function(Null().clone()),
-                                      constraints(std::vector<MinMax<NodePtr> >()),
-                                      states(StateList())
+        std::vector<T> get_vals() const
         {
-
-        }
-        NodePtr objective_function;
-        std::vector<NodePtr> get_constraints() const
-        {
-            std::vector<NodePtr> ret;
-            for (auto c = constraints.begin() ; c != constraints.end() ; ++c)
+            std::vector<T> ret;
+            for (auto c = val.begin() ; c != val.end() ; ++c)
             {
-                ret.push_back(c->val);
+                ret.push_back(*c);
             }
             return ret;
+        }
+
+        void add_to_val(const T& v)
+        {
+            val.push_back(v);
+        }
+
+        void push_back(const Parameter& min_, const T& val_, const Parameter& max_)
+        {
+            add_to_val(val_);
+            min[get_string(val_)] = min_;
+            max[get_string(val_)] = max_;
+        }
+
+        void push_back(const Parameter& min_, const T& val_)
+        {
+            add_to_val(val_);
+            min[get_string(val_)] = min_;
+        }
+
+        void push_back(const T& val_, const Parameter& max_)
+        {
+            add_to_val(val_);
+            (void)max_;
+            max[get_string(val_)] = max_;
         }
 
         double get_max_bound() const
@@ -53,8 +64,83 @@ class OptimizationProblem::OptimizationProblem_pimpl
             return -get_max_bound();
         }
 
-        std::vector<MinMax<NodePtr> > constraints;
-        StateList states;
+        void get_bounds(const size_t& n, double* const l, double* const u) const
+        {
+            if (l == NULL)
+            {
+                THROW("OptimizationProblem::OptimizationProblem_pimpl::get_bounds(double* const, double* const)", OptimizationProblemException, "l == NULL");
+            }
+            if (u == NULL)
+            {
+                THROW("OptimizationProblem::OptimizationProblem_pimpl::get_bounds(double* const, double* const)", OptimizationProblemException, "u == NULL");
+            }
+            if (n!= val.size())
+            {
+                THROW("get_bounds(const size_t&, double* const, double* const)", OptimizationProblemException, "Invalid size");
+            }
+            size_t i = 0;
+            for (auto v = val.begin() ; v != val.end() ; ++v)
+            {
+                auto it_min = min.find(get_string(*v));
+                auto it_max = max.find(get_string(*v));
+                bool found_value_in_min_map = not(it_min==min.end());
+                bool found_value_in_max_map = not(it_max==max.end());
+                l[i]   = found_value_in_min_map ? *(it_min->second) : get_min_bound();
+                u[i++] = found_value_in_max_map ? *(it_max->second) : get_max_bound();
+            }
+        }
+
+        std::string get_string(const T& t) const
+        {
+            std::stringstream ss;
+            Serialize v(ss);
+            t->accept(v);
+            return ss.str();
+        }
+
+    private:
+        std::map<std::string,Parameter> min;
+        std::map<std::string,Parameter> max;
+        std::vector<T> val;
+};
+
+template <> void MinMaxList<StatePtr>::add_to_val(const StatePtr& v)
+{
+    append(val, v);
+}
+
+class OptimizationProblem::OptimizationProblem_pimpl
+{
+    public:
+        OptimizationProblem_pimpl() : objective_function(Null().clone()),
+                                      constraints(MinMaxList<NodePtr>()),
+                                      states(MinMaxList<StatePtr>())
+        {
+
+        }
+
+        std::vector<NodePtr> get_constraints() const
+        {
+            return constraints.get_vals();
+        }
+
+        StateList get_states() const
+        {
+            return states.get_vals();
+        }
+
+        void register_states()
+        {
+            auto s = ::get_states(objective_function, constraints.get_vals());
+            for (auto it = s.begin() ; it != s.end() ; ++it)
+            {
+                states.add_to_val(*it);
+            }
+        }
+
+        NodePtr objective_function;
+        MinMaxList<NodePtr> constraints;
+        MinMaxList<StatePtr> states;
 };
 
 OptimizationProblem::OptimizationProblem() : pimpl(new OptimizationProblem_pimpl())
@@ -64,27 +150,44 @@ OptimizationProblem::OptimizationProblem() : pimpl(new OptimizationProblem_pimpl
 OptimizationProblem& OptimizationProblem::minimize(const NodePtr& objective_function)
 {
     pimpl->objective_function = objective_function;
-    pimpl->states = ::get_states(pimpl->objective_function, pimpl->get_constraints());
+    pimpl->register_states();
     return *this;
 }
 
 OptimizationProblem& OptimizationProblem::subject_to(const double& min_bound, const NodePtr& constraint)
 {
-    pimpl->constraints.push_back(MinMax<NodePtr>(Parameter(min_bound),constraint,Parameter(pimpl->get_max_bound())));
-    pimpl->states = ::get_states(pimpl->objective_function, pimpl->get_constraints());
+    pimpl->constraints.push_back(min_bound, constraint);
+    pimpl->register_states();
     return *this;
 }
 
 OptimizationProblem& OptimizationProblem::subject_to(const double& min_bound, const NodePtr& constraint, const double& max_bound)
 {
-    pimpl->constraints.push_back(MinMax<NodePtr>(Parameter(min_bound),constraint,Parameter(max_bound)));
-    pimpl->states = ::get_states(pimpl->objective_function, pimpl->get_constraints());
+    pimpl->constraints.push_back(min_bound,constraint,max_bound);
+    pimpl->register_states();
+    return *this;
+}
+
+OptimizationProblem& OptimizationProblem::bound_state(const double& min_bound, const StatePtr& state, const double& max_bound)
+{
+    pimpl->states.push_back(min_bound, state, max_bound);
+    return *this;
+}
+
+OptimizationProblem& OptimizationProblem::bound_state(const StatePtr& state, const double& max_bound)
+{
+    pimpl->states.push_back(state, Parameter(max_bound));
+    return *this;
+}
+OptimizationProblem& OptimizationProblem::bound_state(const double& min_bound, const StatePtr& state)
+{
+    pimpl->states.push_back(min_bound, state);
     return *this;
 }
 
 StateList OptimizationProblem::get_states() const
 {
-    return pimpl->states;
+    return pimpl->states.get_vals();
 }
 
 NodePtr OptimizationProblem::get_objective_function() const
@@ -92,20 +195,12 @@ NodePtr OptimizationProblem::get_objective_function() const
     return pimpl->objective_function;
 }
 
-void OptimizationProblem::get_constraint_bounds(double* const gl, double* const gu) const
+void OptimizationProblem::get_constraint_bounds(const size_t& n, double* const gl, double* const gu) const
 {
-    size_t i = 0;
-    if (gl == NULL)
-    {
-        THROW("OptimizationProblem::get_constraint_bounds(double* const, double* const)", OptimizationProblemException, "gl == NULL");
-    }
-    if (gu == NULL)
-    {
-        THROW("OptimizationProblem::get_constraint_bounds(double* const, double* const)", OptimizationProblemException, "gu == NULL");
-    }
-    for (auto it = pimpl->constraints.begin() ; it != pimpl->constraints.end() ; ++it)
-    {
-        gl[i] = *(it->min);
-        gu[i++] = *(it->max);
-    }
+    pimpl->constraints.get_bounds(n, gl, gu);
+}
+
+void OptimizationProblem::get_state_bounds(const size_t& n, double* const xl, double* const xu) const
+{
+    pimpl->states.get_bounds(n, xl, xu);
 }
