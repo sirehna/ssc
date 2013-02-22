b705a6900b589ff20999fcaeb5e6d2c672d2694a
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-02-22 14:27:01 +0000

    Début du travail sur l'optimiseur : création du répertoire "optimizer"
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@294 9358c2ab-9c44-400d-ad1c-7153579a8fc0

---
+++ ssc/optimizer/src/OptimizationProblem.cpp
@@ -0,0 +1,15 @@
+/*
+ * OptimizationProblem.cpp
+ *
+ * \date 21 févr. 2013, 10:15:54
+ *  \author cec
+ */
+
+#include "../inc/OptimizationProblem.hpp"
+
+OptimizationProblem::OptimizationProblem()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
