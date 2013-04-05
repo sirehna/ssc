0eefc32837eec164aaaab654deb0f0ff1fa53ec9
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-04-05 14:28:21 +0000

    - Added LibgGeographic for calculations on the WGS84 (because Boost.geographical doesn't allow us to calculate the coordinates of a point on a track (first geodetic problem). - Renamed module ECA_zone to geometry because it seemed more appropriate. - Added the Weather type which will be used as an interface between the weather module & the speed optimization module.
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@352 9358c2ab-9c44-400d-ad1c-7153579a8fc0

---
+++ ssc/geometry/inc/PointInCartesianPolygon.hpp
@@ -0,0 +1,28 @@
+/*
+ * PointInCartesianPolygon.hpp
+ *
+ * \date 27 mars 2013, 15:56:51
+ *  \author cec
+ */
+
+#ifndef POINTINCARTESIANPOLYGON_HPP_
+#define POINTINCARTESIANPOLYGON_HPP_
+
+#include "PointInPolygon.hpp"
+
+/** \author cec
+ *  \brief This class was created to check if a point is in a polygon defined in Cartesian coordinates.
+ *  \details It's a simple wrapper around boost.geometry.
+ *  \section ex1 Example
+ *  \snippet ECA_zones/unit_tests/src/PointInCartesianPolygonTest.cpp PointInCartesianPolygonTest example
+ *  \section ex2 Expected output
+ *  \snippet ECA_zones/unit_tests/src/PointInCartesianPolygonTest.cpp PointInCartesianPolygonTest expected output
+ */
+
+class PointInCartesianPolygon : public PointInPolygon
+{
+    public:
+        PointInCartesianPolygon(const std::vector<LongitudeLatitude>& polygon);
+};
+
+#endif /* POINTINCARTESIANPOLYGON_HPP_ */
