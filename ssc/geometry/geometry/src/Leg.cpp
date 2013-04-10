6847437c3a093403cf551503feccbcf1c19c91c7
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-04-10 06:04:52 +0000

    Track class works as expected - Fixed test Track::should_be_able_to_find_leg_index_from_distance_on_track : special case for last point. - Added example to Leg::find_waypoint_at
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@360 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/geometry/geometry/src/Leg.cpp
+++ ssc/geometry/geometry/src/Leg.cpp
@@ -49,8 +49,10 @@ double Leg::length() const
 
 /** \author cec
  *  \date 9 avr. 2013, 15:38:04
-  *  \brief Find a waypoint on the leg, at a given distance from the first point
+ *  \brief Find a waypoint on the leg, at a given distance from the first point
  *  \returns Point at given distance from start of leg
+ *  \section ex1 Example
+ *  \snippet geometry/unit_tests/src/LegTest.cpp LegTest find_waypoint_at_example
  */
 LongitudeLatitude Leg::find_waypoint_at(const double& distance //!< Distance from first waypoint (in meters)
                                        ) const
