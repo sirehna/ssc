affbb42254b6938859b68c2e140a5b9649f7fa8d
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-04-18 11:35:57 +0000

    Simplified Doxygen documentation: modules can be created automatically using the \group commands. Implies that the Doxygen Layout file does not need to be modified.
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@384 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/geometry/inc/PointInCartesianPolygon.hpp
+++ ssc/geometry/inc/PointInCartesianPolygon.hpp
@@ -11,6 +11,7 @@
 #include "PointInPolygon.hpp"
 
 /** \author cec
+ *  \ingroup geometry
  *  \brief Checks if a point is in a polygon defined in Cartesian coordinates.
  *  \details It's a simple wrapper around boost.geometry.
  *  \section ex1 Example
@@ -18,7 +19,6 @@
  *  \section ex2 Expected output
  *  \snippet geometry/unit_tests/src/PointInCartesianPolygonTest.cpp PointInCartesianPolygonTest expected output
  */
-
 class PointInCartesianPolygon : public PointInPolygon
 {
     public:
