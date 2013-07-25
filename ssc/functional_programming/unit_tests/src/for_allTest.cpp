bad7d5adebe1a552d091dbdf18ac11e3f68c3f92
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-07-25 13:23:41 +0000

    functional_programming: 'for_each' on a vector
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@592 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/functional_programming/unit_tests/src/for_allTest.cpp
+++ ssc/functional_programming/unit_tests/src/for_allTest.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "for_allTest.hpp"
-
+#include "for_all.hpp"
 
 #include "test_macros.hpp"
 
@@ -26,11 +26,22 @@ void for_allTest::TearDown()
 {
 }
 
-TEST_F(for_allTest, example)
+TEST_F(for_allTest, can_apply_a_function_to_each_element_in_a_vector)
 {
 //! [for_allTest example]
+    const std::vector<double> v({1,3,2,8,7,1,9});
+    std::function<double(const double&)> f = [](const double& x)->double{return x+2;};
+    const std::vector<double> ret = for_all(v, f);
 //! [for_allTest example]
 //! [for_allTest expected output]
+    ASSERT_EQ(v.size(), ret.size());
+    ASSERT_EQ(3, ret.at(0));
+    ASSERT_EQ(5,ret.at(1));
+    ASSERT_EQ(4,ret.at(2));
+    ASSERT_EQ(10,ret.at(3));
+    ASSERT_EQ(9,ret.at(4));
+    ASSERT_EQ(3,ret.at(5));
+    ASSERT_EQ(11,ret.at(6));
 //! [for_allTest expected output]
 }
 
