90b0b55028bc6f026c5a702c298e5c265b1720bc
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-07-25 13:24:25 +0000

    Can apply function of two arguments
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@598 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/functional_programming/unit_tests/src/for_allTest.cpp
+++ ssc/functional_programming/unit_tests/src/for_allTest.cpp
@@ -65,3 +65,17 @@ TEST_F(for_allTest, throws_if_supplied_two_vectors_of_different_lengths)
     ASSERT_THROW(for_all(v1,v2,std::plus<double>()), For_all_exception);
 }
 
+TEST_F(for_allTest, can_apply_function_of_two_arguments_to_all_elements_in_pair_of_vectors)
+{
+    const std::vector<double> v1({1,3,2,8 ,7  ,1,9});
+    const std::vector<double> v2({4,5,8,65,814,2,4});
+    const std::vector<double> ret = for_all(v1,v2,std::plus<double>());
+    ASSERT_EQ(7, ret.size());
+    ASSERT_EQ(5,ret.at(0));
+    ASSERT_EQ(8,ret.at(1));
+    ASSERT_EQ(10,ret.at(2));
+    ASSERT_EQ(73,ret.at(3));
+    ASSERT_EQ(821,ret.at(4));
+    ASSERT_EQ(3,ret.at(5));
+    ASSERT_EQ(13,ret.at(6));
+}
