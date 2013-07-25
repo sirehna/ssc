8fa27b30d6e1d63d3d002040e7fa8d2b513b1cce
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-07-25 13:23:33 +0000

    Added a new module for functional programming
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@591 9358c2ab-9c44-400d-ad1c-7153579a8fc0

---
+++ ssc/functional_programming/unit_tests/src/for_allTest.cpp
@@ -0,0 +1,38 @@
+/*
+ * for_allTest.cpp
+ *
+ * \date 19 juil. 2013, 11:52:33
+ *  \author cec
+ */
+
+#include "for_allTest.hpp"
+
+
+#include "test_macros.hpp"
+
+for_allTest::for_allTest() : a(DataGenerator(11))
+{
+}
+
+for_allTest::~for_allTest()
+{
+}
+
+void for_allTest::SetUp()
+{
+}
+
+void for_allTest::TearDown()
+{
+}
+
+TEST_F(for_allTest, example)
+{
+//! [for_allTest example]
+//! [for_allTest example]
+//! [for_allTest expected output]
+//! [for_allTest expected output]
+}
+
+
+
