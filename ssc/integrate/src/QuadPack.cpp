ec24ff919263c424800a1ed290dc288325fd3578
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-07-26 07:39:11 +0000

    Quadpack now throws an exception describing the error when ier != 0
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@664 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/integrate/src/QuadPack.cpp
+++ ssc/integrate/src/QuadPack.cpp
@@ -74,19 +74,54 @@ double QuadPack::integrate(double a, double b, double eps) const
     int lenw = LENW, limit = LIMIT;
     double res = 0;
     dqags_(quadpack_integrand, (void*)(this), &a, &b, &epsabs, &epsrel, &res, &abserr, &neval, &ier, &limit, &lenw, &last, iwork, work);
+    throw_any_errors(ier);
+    return res;
+}
+
+void QuadPack::throw_any_errors(const int ier) const
+{
     if (ier < 0)
     {
         std::stringstream ss;
         ss << "ier = " << ier << ": possible memory corruption";
         THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
     }
-    if ((ier > 0) && (ier != 2))
+    if (ier == 1)
     {
         std::stringstream ss;
-        ss << "ier = " << ier;
+        ss << "Maximum number of subdivisions allowed has been achieved. One can allow more sub-divisions by increasing the value of limit (and taking the according dimension adjustments into account. However, if this yields no improvement it is advised to analyze the integrand in order to determine the integration difficulties. If the position of a local difficulty can be determined (e.g. singularity, discontinuity within the interval) one will probably gain from splitting up the interval at this point and calling the integrator on the subranges. If possible, an appropriate special-purpose integrator should be used, which is designed for handling the type of difficulty involved.";
         THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
     }
-    return res;
+    if (ier == 2)
+    {
+        std::stringstream ss;
+        ss << "The occurrence of roundoff error is detected, which prevents the requested tolerance from being achieved. The error may be under-estimated.";
+        THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
+    }
+    if (ier == 3)
+    {
+        std::stringstream ss;
+        ss << "Extremely bad integrand behaviour occurs at some points of the integration interval.";
+        THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
+    }
+    if (ier == 4)
+    {
+        std::stringstream ss;
+        ss << "The algorithm does not converge. Roundoff error is detected in the extrapolation table. It is presumed that the requested tolerance cannot be achieved, and that the returned result is the best which can be obtained.";
+        THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
+    }
+    if (ier == 5)
+    {
+        std::stringstream ss;
+        ss << "The integral is probably divergent, or slowly convergent.";
+        THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
+    }
+    if (ier == 6)
+    {
+        std::stringstream ss;
+        ss << "The input is invalid, because (epsabs<=0 and epsrel<max(50*rel.mach.acc.,0.5d-28) or limit<1 or lenw<limit*4.";
+        THROW(__PRETTY_FUNCTION__, QuadPackException, ss.str());
+    }
 }
 
 QuadPack::~QuadPack()
