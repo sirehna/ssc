f4e1dab85be6421671183b4ebe79140748a0c7ee
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2013-08-29 13:23:13 +0000

    Removed unnecessary passage by reference
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/csv_file_reader@1652 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- csv_file_reader/inc/CSVFileReader.hpp
+++ csv_file_reader/inc/CSVFileReader.hpp
@@ -28,10 +28,10 @@ class CSVFileReader
 		CSVFileReader();
 		std::vector<std::string> titles;
 		std::vector<std::vector<double> > values;
-		const std::vector<std::string> convert_line_to_vector_of_strings(const std::string& line, const char& separator) const;
-		const std::vector<double> convert_line_to_vector_of_doubles(const std::string& line, const char& separator) const;
 		void extract_column_titles(std::ifstream& file, const size_t& expected_nb_of_columns, const char& separator);
-		void extract_values(std::ifstream& file, const char& separator);
+		const std::vector<std::string> convert_line_to_vector_of_strings(const std::string& line, const char separator) const;
+		const std::vector<double> convert_line_to_vector_of_doubles(const std::string& line, const char separator) const;
+		void extract_values(std::istream& file, const char separator);
 		std::vector<std::vector<double> >::const_iterator that_line;
 };
 
