f648f97e66cbdf37617e2d5f241ea12570d76fa5
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2013-09-10 14:08:37 +0000

    Prepared files for SplineVariableStep
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/interpolation@1658 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ interpolation/inc/SplineVariableStep.hpp
@@ -0,0 +1,24 @@
+/*
+ * SplineVariableStep.hpp
+ *
+ * \date 9 sept. 2013, 10:47:42
+ *  \author cec
+ */
+
+#ifndef SPLINEVARIABLESTEP_HPP_
+#define SPLINEVARIABLESTEP_HPP_
+
+/** \author cec
+ *  \ingroup interpolation
+ *  \brief Performs a cubic spline interpolation for non-equidistant abscissae
+ *  \section ex1 Example
+ *  \snippet interpolation/unit_tests/src/SplineVariableStepTest.cpp SplineVariableStepTest example
+ *  \section ex2 Expected output
+ *  \snippet interpolation/unit_tests/src/SplineVariableStepTest.cpp SplineVariableStepTest expected output
+ */
+
+class SplineVariableStep
+{
+};
+
+#endif /* SPLINEVARIABLESTEP_HPP_ */
---
+++ interpolation/src/SplineVariableStep.cpp
@@ -0,0 +1,9 @@
+/*
+ * SplineVariableStep.cpp
+ *
+ * \date 9 sept. 2013, 10:47:42
+ *  \author cec
+ */
+
+#include "../inc/SplineVariableStep.hpp"
+
