92165aa2e9d3097c27df0ff74a67e481452ca9a1
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-10-09 10:02:23 +0000

    Created a 'Cumulate' class
    This class performs a trapezoidal integration on scatter data ((x,y)
    pairs). It allows adding points in any order (which of course changes
    the value of the integral) & calculating the integral between any
    (x1,x2) pair.
    
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@939 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/integrate/inc/generic_pimpl.hpp
+++ ssc/integrate/inc/generic_pimpl.hpp
@@ -10,24 +10,18 @@
 
 // From http://herbsutter.com/gotw/_101/
 
-#include <utility>
+#include <memory>
 
-template<typename T>
-pimpl<T>::pimpl() : m{ new T{} } { }
-
-template<typename T>
-template<typename ...Args>
-pimpl<T>::pimpl( Args&& ...args )
-    : m{ new T{ std::forward<Args>(args)... } } { }
-
-template<typename T>
-pimpl<T>::~pimpl() { }
-
-template<typename T>
-T* pimpl<T>::operator->() { return m.get(); }
-
-template<typename T>
-T& pimpl<T>::operator*() { return *m.get(); }
-
+template<typename T> class Pimpl
+{
+    private:
+        std::unique_ptr<T> m;
+    public:
+        Pimpl();
+        template<typename ...Args> Pimpl( Args&& ... );
+        ~Pimpl();
+        T* operator->();
+        T& operator*();
+};
 
 #endif /* GENERIC_PIMPL_HPP_ */
--- ssc/integrate/unit_tests/src/CumulateTest.cpp
+++ ssc/integrate/unit_tests/src/CumulateTest.cpp
@@ -6,11 +6,12 @@
  */
 
 #include "CumulateTest.hpp"
-
+#include "Cumulate.hpp"
+#include "CumulateException.hpp"
 
 #include "test_macros.hpp"
 
-CumulateTest::CumulateTest() : a(DataGenerator(TODO))
+CumulateTest::CumulateTest() : a(DataGenerator(22))
 {
 }
 
@@ -29,6 +30,23 @@ void CumulateTest::TearDown()
 TEST_F(CumulateTest, example)
 {
 //! [CumulateTest example]
+    Cumulate c;
+    c.add(1,1);
+    ASSERT_THROW(c.integrate(a.random<double>(),a.random<double>()),CumulateException);
+    c.add(2,2);
+    ASSERT_DOUBLE_EQ(1.5, c.integrate(1,2));
+    c.add(3,2);
+    ASSERT_DOUBLE_EQ(1.5, c.integrate(1,2));
+    ASSERT_DOUBLE_EQ(2, c.integrate(2,3));
+    ASSERT_DOUBLE_EQ(3.5, c.integrate(1,3));
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        ASSERT_NO_THROW(c.integrate(a.random<double>().between(1,3),a.random<double>().between(1,3)));
+        const double x0 =  a.random<double>().between(1,3);
+        ASSERT_DOUBLE_EQ(0, c.integrate(x0,x0));
+    }
+    c.add(2.5,80);
+    ASSERT_DOUBLE_EQ(42.5, c.integrate(1,3));
 //! [CumulateTest example]
 //! [CumulateTest expected output]
 //! [CumulateTest expected output]
