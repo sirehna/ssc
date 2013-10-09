0ee863b814fe3122e1fcf3e539d73a8ca09c4ae7
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-10-09 10:01:47 +0000

    Stubbed 'Cumulate' class
    This class will be used to calculate the sum (integral) of a variable
    over time. Unlike the other classes in that module (Simpson, Quadpack,
    etc.), it allows adding points to the function being integrated. The
    function is not defined analytically but by (x,y) pairs. The integration
    scheme is trapezoidal integration.
    
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@936 9358c2ab-9c44-400d-ad1c-7153579a8fc0

---
+++ ssc/integrate/src/Cumulate.cpp
@@ -0,0 +1,9 @@
+/*
+ * Cumulate.cpp
+ *
+ * \date 8 oct. 2013, 08:22:31
+ *  \author cec
+ */
+
+#include "../inc/Cumulate.hpp"
+
---
+++ ssc/integrate/unit_tests/src/CumulateTest.cpp
@@ -0,0 +1,38 @@
+/*
+ * CumulateTest.cpp
+ *
+ * \date 8 oct. 2013, 08:22:10
+ *  \author cec
+ */
+
+#include "CumulateTest.hpp"
+
+
+#include "test_macros.hpp"
+
+CumulateTest::CumulateTest() : a(DataGenerator(TODO))
+{
+}
+
+CumulateTest::~CumulateTest()
+{
+}
+
+void CumulateTest::SetUp()
+{
+}
+
+void CumulateTest::TearDown()
+{
+}
+
+TEST_F(CumulateTest, example)
+{
+//! [CumulateTest example]
+//! [CumulateTest example]
+//! [CumulateTest expected output]
+//! [CumulateTest expected output]
+}
+
+
+
