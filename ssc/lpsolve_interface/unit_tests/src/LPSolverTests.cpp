8224394a0c00c3b6a7c5c4bd8723eb35b79a79de
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-11-22 18:13:59 +0000

    Can solve mixed integer problems
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1182 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/lpsolve_interface/unit_tests/src/LPSolverTests.cpp
+++ ssc/lpsolve_interface/unit_tests/src/LPSolverTests.cpp
@@ -34,26 +34,57 @@ TEST_F(LPSolverTests, example)
     StateGenerator generate;
     auto x = generate.state("x");
     auto y = generate.state("y");
-    std::shared_ptr<OptimizationProblem> pb(new OptimizationProblem());
-    pb->minimize((-1)*(143*x+60*y))
+    std::tr1::shared_ptr<OptimizationProblem> pb(new OptimizationProblem());
+    pb->maximize(143*x+60*y)
       .subject_to(120*x + 210*y, 15000)
       .subject_to(110*x + 30*y, 4000)
       .subject_to(x + y, 75 )
       .bound_state(0,x)
       .bound_state(0,y);
-    std::cout << *pb << std::endl;
 //! [LPSolverTests example]
 //! [LPSolverTests expected output]
     LPSolver solver(pb);
 
-    auto res = solver.solve();
-/*
+    auto res = solver.solve({0,0});
+
     ASSERT_DOUBLE_EQ(6315.625, res.value_of_the_objective_function);
     ASSERT_EQ(2, res.state_values.size());
     ASSERT_DOUBLE_EQ(21.875, res.state_values["x"]);
-    ASSERT_DOUBLE_EQ(53.125, res.state_values["y"]);*/
+    ASSERT_DOUBLE_EQ(53.125, res.state_values["y"]);
 //! [LPSolverTests expected output]
 }
 
+TEST_F(LPSolverTests, can_use_lp_solve_on_non_linear_cost)
+{
+    // We don't use the objective function directly: we only use its gradient so
+    // linearizing around (1,1) should give the same result as before
+    StateGenerator generate;
+    auto x = generate.state("x");
+    auto y = generate.state("y");
+    std::tr1::shared_ptr<OptimizationProblem> pb(new OptimizationProblem());
+    pb->maximize(71.5*x*x+30*y*y)
+      .subject_to(120*x + 210*y, 15000)
+      .subject_to(110*x + 30*y, 4000)
+      .subject_to(x + y, 75 )
+      .bound_state(0,x)
+      .bound_state(0,y);
+    LPSolver solver(pb);
 
+    auto res = solver.solve({1,1});
+    ASSERT_DOUBLE_EQ(6315.625, res.value_of_the_objective_function);
+}
 
+TEST_F(LPSolverTests, can_solve_binary_problems)
+{
+    StateGenerator generate;
+    auto x = generate.state("x");
+    auto y = generate.state("y");
+    std::tr1::shared_ptr<OptimizationProblem> pb(new OptimizationProblem());
+    pb->maximize(x-y)
+      .binary(x)
+      .binary(y);
+    LPSolver solver(pb);
+    auto res = solver.solve(a.random_vector_of<double>().of_size(2));
+    ASSERT_EQ(1,res.state_values["x"]);
+    ASSERT_EQ(0,res.state_values["y"]);
+}
