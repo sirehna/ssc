157650bee480d1413b5475d3eb5f0b23b21ce149
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-11-22 18:13:36 +0000

    WIP
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1179 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/lpsolve_interface/src/LPSolver.cpp
+++ ssc/lpsolve_interface/src/LPSolver.cpp
@@ -9,20 +9,23 @@
 #include "OptimizationProblem.hpp"
 #include "lp_lib.h"
 #include "LPSolverException.hpp"
+#include "FunctionMatrix.hpp"
+#include "State.hpp"
 
 class LPSolver::Impl
 {
     public:
-        Impl() : pb(new OptimizationProblem()), Ncol(0), colno(NULL), row(NULL), lp(NULL)
+        Impl() : pb(new OptimizationProblem()), nb_ob_states(0), nb_of_constraints(0), colno(NULL), row(NULL), lp(NULL)
         {
 
         }
 
         Impl(const Impl& rhs) : pb(new OptimizationProblem(*rhs.pb)),
-                                Ncol(pb->get_states().size()),
-                                colno(new int[Ncol]),
-                                row(new REAL[Ncol]),
-                                lp(make_lp(0, Ncol))
+                                nb_ob_states(pb->get_states().size()),
+                                nb_of_constraints(pb->get_constraints().size()),
+                                colno(new int[nb_ob_states]),
+                                row(new REAL[nb_ob_states]),
+                                lp(make_lp(0, nb_ob_states))
         {
 
         }
@@ -32,13 +35,14 @@ class LPSolver::Impl
             if (&rhs != this)
             {
                 pb = std::shared_ptr<OptimizationProblem>(new OptimizationProblem(*rhs.pb));
-                Ncol = pb->get_states().size();
+                nb_ob_states = pb->get_states().size();
+                nb_of_constraints = pb->get_constraints().size();
                 if (lp) delete_lp(lp);
                 if (colno) delete[] colno;
                 if (row) delete[] row;
-                colno = new int[Ncol];
-                row = new REAL[Ncol];
-                lp = make_lp(0, Ncol);
+                colno = new int[nb_ob_states];
+                row = new REAL[nb_ob_states];
+                lp = make_lp(0, nb_ob_states);
             }
             return *this;
         }
@@ -50,21 +54,60 @@ class LPSolver::Impl
             if (row) delete[] row;
         }
         Impl(const std::shared_ptr<OptimizationProblem>& problem) : pb(problem),
-                                                                    Ncol(pb->get_states().size()),
-                                                                    colno(new int[Ncol]),
-                                                                    row(new REAL[Ncol]),
-                                                                    lp(make_lp(0, Ncol))
+                                                                    nb_ob_states(pb->get_states().size()),
+                                                                    nb_of_constraints(pb->get_constraints().size()),
+                                                                    colno(new int[nb_ob_states]),
+                                                                    row(new REAL[nb_ob_states]),
+                                                                    lp(make_lp(0, nb_ob_states))
         {
         }
 
         OptimizationResult solve()
         {
+            set_state_names(pb->get_states());
+            add_constraints();
             return OptimizationResult();
+        }
+
+        void set_state_names(const std::vector<StatePtr>& states)
+        {
+            size_t i = 1;
+            for (auto it = states.begin() ; it != states.end() ; ++it)
+            {
+                char* c = new char[(*it)->get_name().length()];
+                strcpy(c, (*it)->get_name().c_str());
+                set_col_name(lp, i++, c);
+                delete[]c;
+            }
+        }
+
+        void add_constraints()
+        {
+            std::vector<double> gl(nb_of_constraints), gu(nb_of_constraints);
+            pb->get_constraint_bounds(nb_of_constraints, &gl[0], &gu[0]);
+            const FunctionMatrix M = pb->get_constraint_jacobian();
+            set_add_rowmode(lp, TRUE);
+            const size_t n = M.values.size();
+            size_t k = 0;
+            for (size_t i = 0 ; i < nb_of_constraints ; ++i)
+            {
+                size_t p = 0;
+                for (size_t j = 0 ; j < nb_ob_states ; ++j)
+                {
+                    if ((k < n) && (M.row_index[k] == i) && (M.col_index[k] == j))
+                    {
+                        colno[p] = M.col_index[k]+1;
+                        row[p++] = M.values[k]();
+                    }
+                }
+                add_constraintex(lp, p, row, colno, LE, gu[i]);
+            }
         }
 
     private:
         std::shared_ptr<OptimizationProblem> pb;
-        int Ncol;
+        size_t nb_ob_states;
+        size_t nb_of_constraints;
         int *colno;
         REAL *row;
         lprec* lp;
