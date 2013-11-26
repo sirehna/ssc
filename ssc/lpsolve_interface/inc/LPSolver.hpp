39cba89b74970347c779b3d1a8278e80338b8833
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2013-11-26 16:19:57 +0000

    Stubbed lpsolve_interface
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1211 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/lpsolve_interface/inc/LPSolver.hpp
+++ ssc/lpsolve_interface/inc/LPSolver.hpp
@@ -8,12 +8,16 @@
 #ifndef LPSOLVER_HPP_
 #define LPSOLVER_HPP_
 
+<<<<<<< HEAD
 #include <tr1/memory>
+=======
+>>>>>>> Stubbed lpsolve_interface
 #include <memory>
 #include "OptimizationResult.hpp"
 
 class OptimizationProblem;
 
+<<<<<<< HEAD
 
 struct LPSolverParameters
 {
@@ -21,6 +25,8 @@ struct LPSolverParameters
 	size_t verbose_level;
 };
 
+=======
+>>>>>>> Stubbed lpsolve_interface
 /** \author cec
  *  \ingroup lp_solve_interface
  *  \brief Responsibility
@@ -33,12 +39,20 @@ struct LPSolverParameters
 class LPSolver
 {
     public:
+<<<<<<< HEAD
         LPSolver(const std::tr1::shared_ptr<OptimizationProblem>& problem, const LPSolverParameters& par = LPSolverParameters());
+=======
+        LPSolver(const std::shared_ptr<OptimizationProblem>& problem);
+>>>>>>> Stubbed lpsolve_interface
         LPSolver();
         ~LPSolver();
         LPSolver(const LPSolver& rhs);
         LPSolver& operator=(const LPSolver& rhs);
+<<<<<<< HEAD
         OptimizationResult solve(const std::vector<double>& x0);
+=======
+        OptimizationResult solve();
+>>>>>>> Stubbed lpsolve_interface
 
     private:
         class Impl;
