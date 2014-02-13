46c16fb1ae3da6102c526d6973246bb68026caf3
cec <cec@5574b880-4a8e-4d59-90b7-38b80912c59c>
2014-02-13 14:13:48 +0000

    Fixed encoding issues
    git-svn-id: http://130.66.124.6/svn/dev_pilotage/simulator_sdk/trunk/data_source@473 5574b880-4a8e-4d59-90b7-38b80912c59c

--- data_source/src/SignalContainer.cpp
+++ data_source/src/SignalContainer.cpp
@@ -23,7 +23,6 @@ vector_convertible_types(ConvertibleTypes())
 
 }
 
-
 template <> void SignalContainer::coerce_type<PhysicalQuantity>(std::list<double>& ret) const
 {
 for (ConvertibleTypesIterator it  = scalar_convertible_types.iter_phys_qty.begin() ; it != scalar_convertible_types.iter_phys_qty.end() ; ++it)
@@ -90,7 +89,7 @@ std::vector<std::string> SignalContainer::get_all_signal_names() const
 }
 
 /** \author cec
- *  \date 27 août 2013, 10:34:20
+ *  \date 27 aoÃ»t 2013, 10:34:20
  *  \brief Get the number of signals.
  *  \returns Number of signals in container.
  *  \snippet data_source/unit_tests/src/SignalContainerTest.cpp SignalContainerTest SignalContainer::size_example
