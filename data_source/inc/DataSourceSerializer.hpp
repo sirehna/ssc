27b967ab8e40554f536add8b32f94d69851ea9e3
cec <cec@5574b880-4a8e-4d59-90b7-38b80912c59c>
2014-03-04 11:04:05 +0000

    Stubbed DataSourceSerializer
    git-svn-id: http://130.66.124.6/svn/dev_pilotage/simulator_sdk/trunk/data_source@492 5574b880-4a8e-4d59-90b7-38b80912c59c

---
+++ data_source/inc/DataSourceSerializer.hpp
@@ -0,0 +1,17 @@
+/*
+ * DataSourceSerializer.hpp
+ *
+ *  Created on: 26 févr. 2014
+ *      Author: maroff
+ */
+
+#ifndef DATASOURCESERIALIZER_HPP_
+#define DATASOURCESERIALIZER_HPP_
+
+class DataSourceSerializer {
+public:
+	DataSourceSerializer();
+	virtual ~DataSourceSerializer();
+};
+
+#endif /* DATASOURCESERIALIZER_HPP_ */
