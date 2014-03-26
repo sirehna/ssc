58daa6824d5646d9f6dd6a5b4efb11e3595ff575
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2014-03-26 14:18:01 +0000

    Empty lines
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/text_file_reader@1691 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- text_file_reader/unit_tests/src/TextFileReaderTest.cpp
+++ text_file_reader/unit_tests/src/TextFileReaderTest.cpp
@@ -35,46 +35,46 @@ void TextFileReaderTest::TearDown()
 TEST_F(TextFileReaderTest, example)
 {
 //! [TextFileReaderTest example]
-	char filename1 [L_tmpnam];
-	char filename2 [L_tmpnam];
-	tmpnam (filename1);
-	tmpnam (filename2);
-	std::ofstream file1 (filename1);
-	const std::string contents_file1 = a.random<std::string>();
-	const std::string contents_file2 = a.random<std::string>();
-	file1 << contents_file1;
-	file1.close();
-	std::ofstream file2 (filename2);
-	file2 << contents_file2;
-	file2.close();
-	std::vector<std::string> filenames;
-	filenames.push_back(filename1);
-	filenames.push_back(filename2);
-	TextFileReader reader(filenames);
-	remove (filename1);
-	remove (filename2);
+    char filename1 [L_tmpnam];
+    char filename2 [L_tmpnam];
+    tmpnam (filename1);
+    tmpnam (filename2);
+    std::ofstream file1 (filename1);
+    const std::string contents_file1 = a.random<std::string>();
+    const std::string contents_file2 = a.random<std::string>();
+    file1 << contents_file1;
+    file1.close();
+    std::ofstream file2 (filename2);
+    file2 << contents_file2;
+    file2.close();
+    std::vector<std::string> filenames;
+    filenames.push_back(filename1);
+    filenames.push_back(filename2);
+    TextFileReader reader(filenames);
+    remove (filename1);
+    remove (filename2);
 //! [TextFileReaderTest example]
 //! [TextFileReaderTest expected output]
-	ASSERT_EQ(contents_file1+contents_file2, reader.get_contents());
+    ASSERT_EQ(contents_file1+contents_file2, reader.get_contents());
 //! [TextFileReaderTest expected output]
 }
 
 TEST_F(TextFileReaderTest, should_represent_newlines_correctly)
 {
-	char filename [L_tmpnam];
-	tmpnam (filename);
-	std::ofstream file (filename);
-	const std::string contents_file = a.random<std::string>()()
-			                         + std::string("\n")
-	                                 + a.random<std::string>()()
-	                                 + std::string("\n")
-	                                 + a.random<std::string>()();
-	file << contents_file;
-	file.close();
-	std::vector<std::string> filenames;
-	filenames.push_back(filename);
-	TextFileReader reader(filenames);
-	remove (filename);
-	ASSERT_EQ(contents_file, reader.get_contents());
+    char filename [L_tmpnam];
+    tmpnam (filename);
+    std::ofstream file (filename);
+    const std::string contents_file = a.random<std::string>()()
+                                             + std::string("\n")
+                                     + a.random<std::string>()()
+                                     + std::string("\n")
+                                     + a.random<std::string>()();
+    file << contents_file;
+    file.close();
+    std::vector<std::string> filenames;
+    filenames.push_back(filename);
+    TextFileReader reader(filenames);
+    remove (filename);
+    ASSERT_EQ(contents_file, reader.get_contents());
 }
 
