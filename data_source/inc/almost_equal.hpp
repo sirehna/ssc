583a44d229b3b8dfb9e4d024d5451dc40cdd4936
cec <cec@5574b880-4a8e-4d59-90b7-38b80912c59c>
2014-04-23 12:25:08 +0000

    Added defaulted epsilon for almost_equal
    By default, almost_equal compares to 4 ULP (cf google test) but if eps is
    provided, it will check that the difference between the two numbers is
    less than eps.
    
    git-svn-id: http://130.66.124.6/svn/dev_pilotage/simulator_sdk/trunk/data_source@525 5574b880-4a8e-4d59-90b7-38b80912c59c

--- data_source/inc/almost_equal.hpp
+++ data_source/inc/almost_equal.hpp
@@ -12,17 +12,17 @@
 #include <vector>
 
 template <class T>
-bool almost_equal(const T& , const T& )
+bool almost_equal(const T& , const T&, const double eps=0)
 {
+    (void) eps;
     return false;
 }
 
-template <> bool almost_equal(const double& t1, const double& t2);
-template <> bool almost_equal(const float& t1, const float& t2);
-template <> bool almost_equal(const int& t1, const int& t2);
-template <> bool almost_equal(const size_t& t1, const size_t& t2);
-template <> bool almost_equal(const std::string& t1, const std::string& t2);
-template <> bool almost_equal(const std::vector<double>& t1, const std::vector<double>& t2);
-
+template <> bool almost_equal(const double& t1, const double& t2, const double eps);
+template <> bool almost_equal(const float& t1, const float& t2, const double eps);
+template <> bool almost_equal(const int& t1, const int& t2, const double eps);
+template <> bool almost_equal(const size_t& t1, const size_t& t2, const double eps);
+template <> bool almost_equal(const std::string& t1, const std::string& t2, const double eps);
+template <> bool almost_equal(const std::vector<double>& t1, const std::vector<double>& t2, const double eps);
 
 #endif /* ALMOST_EQUAL_HPP_ */
--- data_source/src/almost_equal.cpp
+++ data_source/src/almost_equal.cpp
@@ -7,40 +7,43 @@
 
 #include "almost_equal.hpp"
 #include "gtest/internal/gtest-internal.h"
+#include <cmath>
 
-template <> bool almost_equal(const double& t1, const double& t2)
+template <> bool almost_equal(const double& t1, const double& t2, const double eps)
 {
+    if (eps>0) return fabs(t1-t2)<eps;
     const testing::internal::FloatingPoint<double> lhs(t1), rhs(t2);
     return lhs.AlmostEquals(rhs);
 }
 
-template <> bool almost_equal(const float& t1, const float& t2)
+template <> bool almost_equal(const float& t1, const float& t2, const double eps)
 {
+    if (eps>0) return fabs(t1-t2)<eps;
     const testing::internal::FloatingPoint<float> lhs(t1), rhs(t2);
     return lhs.AlmostEquals(rhs);
 }
-template <> bool almost_equal(const int& t1, const int& t2)
+template <> bool almost_equal(const int& t1, const int& t2, const double)
 {
     return t1 == t2;
 }
 
-template <> bool almost_equal(const size_t& t1, const size_t& t2)
+template <> bool almost_equal(const size_t& t1, const size_t& t2, const double)
 {
     return t1 == t2;
 }
 
-template <> bool almost_equal(const std::string& t1, const std::string& t2)
+template <> bool almost_equal(const std::string& t1, const std::string& t2, const double)
 {
     return t1 == t2;
 }
 
-template <> bool almost_equal(const std::vector<double>& t1, const std::vector<double>& t2)
+template <> bool almost_equal(const std::vector<double>& t1, const std::vector<double>& t2, const double eps)
 {
     const size_t n = t1.size();
     if (t2.size() != n) return false;
     for (size_t i = 0 ; i < n ; ++i)
     {
-        if (not(almost_equal(t1[i],t2[i]))) return false;
+        if (not(almost_equal(t1[i],t2[i],eps))) return false;
     }
     return true;
 }
