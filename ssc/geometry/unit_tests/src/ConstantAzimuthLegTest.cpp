f9414709b649ab1a86c1f510a39b3fdabab7e130
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2014-05-16 14:27:02 +0000

    Stubbed ConsantAzimuthLeg.
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1261 9358c2ab-9c44-400d-ad1c-7153579a8fc0

---
+++ ssc/geometry/unit_tests/src/ConstantAzimuthLegTest.cpp
@@ -0,0 +1,36 @@
+/*
+ * ConstantAzimuthLegTest.cpp
+ *
+ *  Created on: May 14, 2014
+ *      Author: cady
+ */
+
+#include "ConstantAzimuthLegTest.hpp"
+#include "ConstantAzimuthLeg.hpp"
+
+ConstantAzimuthLegTest::ConstantAzimuthLegTest() : a(DataGenerator(121))
+{
+}
+
+ConstantAzimuthLegTest::~ConstantAzimuthLegTest()
+{
+}
+
+void ConstantAzimuthLegTest::SetUp()
+{
+}
+
+void ConstantAzimuthLegTest::TearDown()
+{
+}
+
+TEST_F(ConstantAzimuthLegTest, example)
+{
+//! [ConstantAzimuthLegTest example]
+//! [ConstantAzimuthLegTest example]
+//! [ConstantAzimuthLegTest expected output]
+//! [ConstantAzimuthLegTest expected output]
+}
+
+
+
