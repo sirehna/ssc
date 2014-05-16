a643cb31b1d134ad0f7c47933ffe63ecaadf106c
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2014-05-16 14:26:42 +0000

    Moved find_waypoint_at to Leg
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1256 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/geometry/geometry/src/Leg.cpp
+++ ssc/geometry/geometry/src/Leg.cpp
@@ -6,15 +6,42 @@
  */
 
 #include "Leg.hpp"
+#include <sstream>
 
-Leg::Leg()
+#define EPS 1e-6
+
+Leg::Leg() : length_(0)
 {
-    // TODO Auto-generated constructor stub
 
 }
 
 Leg::~Leg()
 {
-    // TODO Auto-generated destructor stub
 }
 
+/** \author cec
+ *  \date 9 avr. 2013, 15:38:04
+ *  \brief Find a waypoint on the leg, at a given distance from the first point
+ *  \returns Point at given distance from start of leg
+ *  \section ex1 Example
+ *  \snippet geometry/unit_tests/src/LegTest.cpp LegTest find_waypoint_at_example
+ */
+LatitudeLongitude Leg::find_waypoint_at(const double distance //!< Distance from first waypoint (in meters)
+                                       ) const
+{
+    if (distance>(length_+EPS))
+    {
+        std::stringstream ss;
+        ss << "Asked to find waypoint at d = "
+           << distance
+           << " m from start of leg but leg is only "
+           << length_
+           << " m long (distance - length = " << distance - length_ << " m)";
+        THROW(__PRETTY_FUNCTION__, LegException, ss.str());
+    }
+    if (distance<(-EPS))
+    {
+        THROW(__PRETTY_FUNCTION__, LegException, "received a negative distance");
+    }
+    return waypoint(std::max(std::min(length_,distance),0.));
+}
