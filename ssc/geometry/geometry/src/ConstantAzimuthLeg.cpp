06ae6df8085307fa8918b8ed0e881ce253b54e2e
cec <cec@9358c2ab-9c44-400d-ad1c-7153579a8fc0>
2014-05-16 14:27:13 +0000

    Implemented ConstantAzimuthLeg (except find_closest_point_to)
    git-svn-id: http://130.66.124.6/svn/eonav/oad_and_monitoring_modules@1265 9358c2ab-9c44-400d-ad1c-7153579a8fc0

--- ssc/geometry/geometry/src/ConstantAzimuthLeg.cpp
+++ ssc/geometry/geometry/src/ConstantAzimuthLeg.cpp
@@ -6,5 +6,41 @@
  */
 
 #include "ConstantAzimuthLeg.hpp"
+#include "loxodrome_on_ellipsoid.hpp"
+#include <cmath>
 
+#define PI (4.*atan(1.))
+#define RAD (PI/180.)
+#define DEG (180./PI)
+
+ConstantAzimuthLeg::ConstantAzimuthLeg(const LatitudeLongitude& point1, const LatitudeLongitude& point2, const double L, const double az12_) : Leg(point1,point2,L),
+az12(az12_), latitude_point_1_in_radians(point_1.lat*RAD), longitude_point_1_in_radians(point_1.lon*RAD)
+{
+}
+
+ConstantAzimuthLeg ConstantAzimuthLeg::build(const LatitudeLongitude& point1, const LatitudeLongitude& point2)
+{
+    double L = 0;
+    double az12 = 0;
+    loxodrome_inverse(point1.lat*RAD, point1.lon*RAD, point2.lat*RAD, point2.lon*RAD, L, az12);
+    return ConstantAzimuthLeg(point1,point2,L,az12);
+}
+
+Angle ConstantAzimuthLeg::azimuth_at(const double ) const
+{
+    return Angle::radian(az12);
+}
+
+LatitudeLongitude ConstantAzimuthLeg::find_closest_point_to(const LatitudeLongitude& point) const
+{
+    return point;
+}
+
+LatitudeLongitude ConstantAzimuthLeg::waypoint(const double distance_from_point1) const
+{
+    double lat = 0;
+    double lon = 0;
+    loxodrome_direct(latitude_point_1_in_radians, longitude_point_1_in_radians, lat, lon, distance_from_point1, az12);
+    return LatitudeLongitude(lat*DEG, lon*DEG);
+}
 
