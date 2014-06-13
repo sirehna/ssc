1829222a9540793ad0f4dbd762d82dac1afdfeec
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2014-06-13 06:53:29 +0000

    Added tr1_macros.hpp
    Because clang does not include <tr1/memory> but <memory>
    and then uses std::shared_ptr instead of std::tr1::shared_ptr,
    unlike gcc.
    
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/macros@1709 300066c3-8af3-463d-8cc0-c0233cc64ac1

---
+++ macros/tr1_macros.hpp
@@ -0,0 +1,53 @@
+/*
+ * smart_pointers_macros.hpp
+ *
+ *  Created on: Jun 12, 2014
+ *      Author: cady
+ */
+
+#ifndef TR1_MACROS_HPP_
+#define TR1_MACROS_HPP_
+
+#if defined(__clang__)
+    /* Clang/LLVM. ---------------------------------------------- */
+    #define TR1INC(x) <x>
+    #define TR1(x) std::x
+    #define TR1NAMESPACE 0
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
+    /* Intel ICC/ICPC. ------------------------------------------ */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(__GNUC__) || defined(__GNUG__)
+    /* GNU GCC/G++. --------------------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(__HP_cc) || defined(__HP_aCC)
+    /* Hewlett-Packard C/aC++. ---------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(__IBMC__) || defined(__IBMCPP__)
+    /* IBM XL C/C++. -------------------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(_MSC_VER)
+    /* Microsoft Visual Studio. --------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(__PGI)
+    /* Portland Group PGCC/PGCPP. ------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+    /* Oracle Solaris Studio. ----------------------------------- */
+    #define TR1INC(x) <tr1/x>
+    #define TR1(x) std::tr1::x
+    #define TR1NAMESPACE 1
+#endif
+
+#endif /* TR1_MACROS_HPP_ */
