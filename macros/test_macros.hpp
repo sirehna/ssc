8284f84f1be814c3dca99bdf031d9bd337c733b4
cec <cec@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2014-07-03 07:37:37 +0000

    Added CERR macro the equivalent of COUT macro on standard error
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/macros@1725 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- macros/test_macros.hpp
+++ macros/test_macros.hpp
@@ -12,5 +12,6 @@
 #define QUOTE_ME(x) #x
 #define QUOTE(x) QUOTE_ME(x)
 #define COUT(val) std::cout << "in file " << __FILE__ << ":" << __LINE__ << " " << #val << " = " << val << std::endl;
+#define CERR(val) std::cerr << "in file " << __FILE__ << ":" << __LINE__ << " " << #val << " = " << val << std::endl;
 
 #endif /* TEST_MACROS_HPP */
