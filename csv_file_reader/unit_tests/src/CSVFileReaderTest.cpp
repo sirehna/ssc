df0e87260db7d7dd4097067ab792c2f02545769d
gj@300066c3-8af3-463d-8cc0-c0233cc64ac1 <gj@300066c3-8af3-463d-8cc0-c0233cc64ac1>
2014-07-04 07:48:12 +0000

    Fixed a compilation warning for function tmpnam
    Output value of this function was not used...
    
    [ 91%] Building CXX object csv_file_reader/unit_tests/CMakeFiles/csv_file_reader_tests.dir/src/CSVFileReaderTest.cpp.o
    /home/jacquenot/Work/git/SimulateurIrtJv/code/csv_file_reader/unit_tests/src/CSVFileReaderTest.cpp: In member function ‘virtual void CSVFileReaderTest_example_Test::TestBody()’:
    /home/jacquenot/Work/git/SimulateurIrtJv/code/csv_file_reader/unit_tests/src/CSVFileReaderTest.cpp:34:19: error: ignoring return value of ‘char* tmpnam(char*)’, declared with attribute warn_unused_result [-Werror=unused-result]
    compilation terminated due to -Wfatal-errors.
    cc1plus: all warnings being treated as errors
    make[2]: *** [csv_file_reader/unit_tests/CMakeFiles/csv_file_reader_tests.dir/src/CSVFileReaderTest.cpp.o] Erreur 1
    make[1]: *** [csv_file_reader/unit_tests/CMakeFiles/csv_file_reader_tests.dir/all] Erreur 2
    make: *** [all] Erreur 2
    Running tests...
    
    git-svn-id: http://130.66.124.6/svn/maroff/Product/Cpp/Tools/csv_file_reader@1726 300066c3-8af3-463d-8cc0-c0233cc64ac1

--- csv_file_reader/unit_tests/src/CSVFileReaderTest.cpp
+++ csv_file_reader/unit_tests/src/CSVFileReaderTest.cpp
@@ -30,39 +30,39 @@ void CSVFileReaderTest::TearDown()
 TEST_F(CSVFileReaderTest, example)
 {
 //! [CSVFileReaderTest example]
-	char filename [L_tmpnam];
-	tmpnam (filename);
-	std::ofstream file (filename);
-	const std::string contents = "A,B,C\n1,2,3\n4,5,6\n7,8,9";
-	file << contents;
-	file.close();
-	const size_t invalid_nb_of_columns = a.random<size_t>().but_not(3);
-	ASSERT_THROW(CSVFileReader(filename, invalid_nb_of_columns),CSVFileReaderException);
-	CSVFileReader validator(filename, 3);
-	remove (filename);
+    char filename [L_tmpnam];
+    ASSERT_STRNE(tmpnam(filename),NULL);
+    std::ofstream file(filename);
+    const std::string contents = "A,B,C\n1,2,3\n4,5,6\n7,8,9";
+    file << contents;
+    file.close();
+    const size_t invalid_nb_of_columns = a.random<size_t>().but_not(3);
+    ASSERT_THROW(CSVFileReader(filename, invalid_nb_of_columns),CSVFileReaderException);
+    CSVFileReader validator(filename, 3);
+    remove(filename);
 //! [CSVFileReaderTest example]
 //! [CSVFileReaderTest expected output]
-	std::vector<double> current_line;
-	ASSERT_TRUE(validator.has_more_data());
-	current_line = validator.get_line();
-	ASSERT_EQ(3,current_line.size());
-	ASSERT_EQ(1, current_line.at(0));
-	ASSERT_EQ(2, current_line.at(1));
-	ASSERT_EQ(3, current_line.at(2));
-	ASSERT_TRUE(validator.has_more_data());
-	current_line = validator.get_line();
-	ASSERT_EQ(3,current_line.size());
-	ASSERT_EQ(4, current_line.at(0));
-	ASSERT_EQ(5, current_line.at(1));
-	ASSERT_EQ(6, current_line.at(2));
-	ASSERT_TRUE(validator.has_more_data());
-	current_line = validator.get_line();
-	ASSERT_EQ(3,current_line.size());
-	ASSERT_EQ(7, current_line.at(0));
-	ASSERT_EQ(8, current_line.at(1));
-	ASSERT_EQ(9, current_line.at(2));
-	ASSERT_FALSE(validator.has_more_data());
-	ASSERT_THROW(validator.get_line(),CSVFileReaderException);
+    std::vector<double> current_line;
+    ASSERT_TRUE(validator.has_more_data());
+    current_line = validator.get_line();
+    ASSERT_EQ(3,current_line.size());
+    ASSERT_EQ(1, current_line.at(0));
+    ASSERT_EQ(2, current_line.at(1));
+    ASSERT_EQ(3, current_line.at(2));
+    ASSERT_TRUE(validator.has_more_data());
+    current_line = validator.get_line();
+    ASSERT_EQ(3,current_line.size());
+    ASSERT_EQ(4, current_line.at(0));
+    ASSERT_EQ(5, current_line.at(1));
+    ASSERT_EQ(6, current_line.at(2));
+    ASSERT_TRUE(validator.has_more_data());
+    current_line = validator.get_line();
+    ASSERT_EQ(3,current_line.size());
+    ASSERT_EQ(7, current_line.at(0));
+    ASSERT_EQ(8, current_line.at(1));
+    ASSERT_EQ(9, current_line.at(2));
+    ASSERT_FALSE(validator.has_more_data());
+    ASSERT_THROW(validator.get_line(),CSVFileReaderException);
 //! [CSVFileReaderTest expected output]
 }
 
