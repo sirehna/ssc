596da3390083f702dcbd4c91e1e4bed2e21c0f09
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-22 12:52:41 +0200

    Put numeric in ssc::numeric

--- numeric/unit_tests/src/pairwise_sumTest.cpp
+++ numeric/unit_tests/src/pairwise_sumTest.cpp
@@ -8,6 +8,8 @@
 #include "pairwise_sumTest.hpp"
 #include "pairwise_sum.hpp"
 
+using namespace ssc::numeric;
+
 pairwise_sumTest::pairwise_sumTest() : a(DataGenerator(451212))
 {
 }
@@ -27,48 +29,48 @@ void pairwise_sumTest::TearDown()
 TEST_F(pairwise_sumTest, example0)
 {
     const std::vector<int> v;
-    ASSERT_EQ(0, sum::pairwise(v));
+    ASSERT_EQ(0, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example1)
 {
     const std::vector<int> v = {456};
-    ASSERT_EQ(456, sum::pairwise(v));
+    ASSERT_EQ(456, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example2)
 {
     const std::vector<int> v = {1,2};
-    ASSERT_EQ(3, sum::pairwise(v));
+    ASSERT_EQ(3, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example3)
 {
     const std::vector<int> v = {1,2,3};
-    ASSERT_EQ(6, sum::pairwise(v));
+    ASSERT_EQ(6, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example4)
 {
     const std::vector<int> v = {1,2,3,4};
-    ASSERT_EQ(10, sum::pairwise(v));
+    ASSERT_EQ(10, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example5)
 {
     const std::vector<int> v = {1,2,3,4,5};
-    ASSERT_EQ(15, sum::pairwise(v));
+    ASSERT_EQ(15, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example14)
 {
     const std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    ASSERT_EQ(105, sum::pairwise(v));
+    ASSERT_EQ(105, pairwise(v));
 }
 
 TEST_F(pairwise_sumTest, example_with_index)
 {
     const std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
     const std::vector<size_t> idx = {0,1,2,3};
-    ASSERT_EQ(10, sum::pairwise(idx,v));
+    ASSERT_EQ(10, pairwise(idx,v));
 }
