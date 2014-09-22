e0d5f5efa28fcf447268e55d5f5422071f67f3d0
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-22 14:47:14 +0200

    Put 'yaml_parser' in ssc::yaml_parser

--- yaml_parser/inc/YamlParser.hpp
+++ yaml_parser/inc/YamlParser.hpp
@@ -1,7 +1,7 @@
 /*
  * YamlParser.hpp
  *
- * \date 27 août 2012, 13:05:23
+ * \date 27 aoï¿½t 2012, 13:05:23
  *  \author cec
  */
 
@@ -15,18 +15,23 @@ namespace YAML
     class Node;
 }
 
-class YamlParser
+namespace ssc
 {
-	public:
-	virtual ~YamlParser() {}
-	YamlParser(const std::string& contents_to_parse);
+    namespace yaml_parser
+    {
+        class YamlParser
+        {
+            public:
+            virtual ~YamlParser() {}
+            YamlParser(const std::string& contents_to_parse);
 
 
-	protected:
-	    YAML::Node& convert_stream_to_yaml_node(const std::string& input_data, YAML::Node& node) const;
-		YamlParser();
-		const std::string contents;
-};
-
+            protected:
+                YAML::Node& convert_stream_to_yaml_node(const std::string& input_data, YAML::Node& node) const;
+                YamlParser();
+                const std::string contents;
+        };
+    }
+}
 
 #endif /* YAMLPARSER_HPP_ */
