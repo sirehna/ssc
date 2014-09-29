11f8d33f6e845912f89fbbea906f587130e3dcca
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-29 10:29:34 +0200

    Can check that two numbers are relatively different
    Opposite of ASSERT_SMALL_RElATIVE_ERROR

--- ssc/macros/extra_test_assertions.hpp
+++ ssc/macros/extra_test_assertions.hpp
@@ -10,5 +10,6 @@
 
 #define ASSERT_SMALL_RELATIVE_ERROR(expected, actual, tol) ASSERT_NEAR(expected, actual, fabs(expected)*tol > tol ? fabs(expected)*tol:tol)
 #define EXPECT_SMALL_RELATIVE_ERROR(expected, actual, tol) EXPECT_NEAR(expected, actual, fabs(expected)*tol > tol ? fabs(expected)*tol:tol)
+#define ASSERT_BIG_RELATIVE_ERROR(expected, actual, tol) ASSERT_GT(fabs(expected - actual), fabs(expected)*tol > tol ? fabs(expected)*tol:tol)
 
 #endif /* EXTRA_TEST_ASSERTIONS_HPP_ */
