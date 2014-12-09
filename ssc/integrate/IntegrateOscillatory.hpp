3364a5010bc5baef70bb0fb4374337b6403fb94e
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-09 16:37:09 +0100

    Renamed class IntegrateOscillatory -> ClenshawCurtis

--- ssc/integrate/IntegrateOscillatory.hpp
+++ ssc/integrate/IntegrateOscillatory.hpp
@@ -14,14 +14,28 @@ namespace ssc
 {
     namespace integrate
     {
-        class IntegrateOscillatory : public QuadPack
+        class ClenshawCurtis : public QuadPack
         {
             public:
-                IntegrateOscillatory();
-                IntegrateOscillatory(const Function& f_);
+                ClenshawCurtis(const double tau);
+                ClenshawCurtis(const Function& f, const double tau);
+                ~ClenshawCurtis();
+                ClenshawCurtis(const ClenshawCurtis& rhs);
+                ClenshawCurtis& operator=(const ClenshawCurtis& rhs);
 
             private:
+                ClenshawCurtis();
                 double integrate_impl(const Function& f, double a, double b, double eps=1e-6);
+                double tau;
+                int nb_of_chebychev_moments;
+                int nb_of_calls;
+                double *chebychev_moments;
+                double *alist;
+                double *blist;
+                double *rlist;
+                double *elist;
+                int *iord;
+                int *nnlog;
         };
     }
 }
--- ssc/integrate/unit_tests/src/IntegrateOscillatoryTest.cpp
+++ ssc/integrate/unit_tests/src/IntegrateOscillatoryTest.cpp
@@ -9,6 +9,10 @@
 #include "IntegrateOscillatoryTest.hpp"
 #include "ssc/integrate/IntegrateOscillatory.hpp"
 
+#define _USE_MATH_DEFINE
+#include <cmath>
+#define PI M_PI
+
 IntegrateOscillatoryTest::IntegrateOscillatoryTest() : a(ssc::random_data_generator::DataGenerator(54212))
 {
 }
@@ -28,9 +32,12 @@ void IntegrateOscillatoryTest::TearDown()
 TEST_F(IntegrateOscillatoryTest, example)
 {
 //! [IntegrateOscillatoryTest example]
-    ssc::integrate::IntegrateOscillatory I([](const double omega){return log(omega);});
+    ssc::integrate::ClenshawCurtis I([](const double omega){return log(omega);}, 10*PI);
 //! [IntegrateOscillatoryTest example]
 //! [IntegrateOscillatoryTest expected output]
+    const double expected = -0.1281316; // - ( gamma + log(10*PI) - ci(10*pi) ) / (10*pi)
+
+    ASSERT_NEAR(expected, I.integrate_f(0,1,1E-3), 1E-7);
 //! [IntegrateOscillatoryTest expected output]
 }
 
