c161145462c37f1eb81acb45d61a4b8edb787960
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-11 09:46:53 +0100

    Stubbed Burcher's integration method

---
+++ ssc/integrate/unit_tests/inc/BurcherTest.hpp
@@ -0,0 +1,25 @@
+/*
+ * BurcherTest.hpp
+ *
+ *  Created on: Dec 11, 2014
+ *      Author: cady
+ */
+
+
+#ifndef BURCHERTEST_HPP_
+#define BURCHERTEST_HPP_
+
+#include "gtest/gtest.h"
+#include <ssc/random_data_generator/DataGenerator.hpp>
+
+class BurcherTest : public ::testing::Test
+{
+    protected:
+        BurcherTest();
+        virtual ~BurcherTest();
+        virtual void SetUp();
+        virtual void TearDown();
+        ssc::random_data_generator::DataGenerator a;
+};
+
+#endif  /* BURCHERTEST_HPP_ */
