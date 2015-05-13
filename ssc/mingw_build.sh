e8721eaed64b9ef4ab70880321de97707cd4270b
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-05-13 14:40:53 +0200

    [master] Updated shell script for compilation

--- ssc/mingw_build.sh
+++ ssc/mingw_build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+if [ $# -eq 0 ]
+then
+    buildType=Debug
+else
+    buildType=$1
+fi
+
 mkdir build
 ./foreach.sh externalDependencies.txt fetch_rebase.sh
-cd internal_solver_validation
-./foreach.sh externalDependencies.txt fetch_rebase.sh
 cd build
-cmake .. -G "MSYS Makefiles" -DCMAKE_COLOR_MAKEFILE=OFF -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "MSYS Makefiles" -DCMAKE_COLOR_MAKEFILE=OFF -DCMAKE_BUILD_TYPE=${buildType}
 cd ..
