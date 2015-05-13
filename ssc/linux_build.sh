f2d6feba60bab58e835cd66f1f43ce5d1362ef44
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-05-13 14:40:53 +0200

    [master] Updated shell script for compilation

--- ssc/linux_build.sh
+++ ssc/linux_build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
+if [ $# -eq 0 ]
+then
+    buildType=Debug
+else
+    buildType=$1
+fi
+
 mkdir build
 ./foreach.sh externalDependencies.txt svn_export.sh $1 $2
-cd internal_solver_validation
-./foreach.sh externalDependencies.txt svn_export.sh $1 $2
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=${buildType}
 cd ..
