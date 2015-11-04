6994d78933c3c0d04881904dc07594c47f4be761
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-11-04 10:51:45 +0100

    Refactored TextFileReaderTest

--- ssc/text_file_reader/unit_tests/src/TextFileReaderTest.cpp
+++ ssc/text_file_reader/unit_tests/src/TextFileReaderTest.cpp
@@ -38,21 +38,25 @@ inline void remove_file_if_it_exists (const std::string& name)
   if (stat (name.c_str(), &buffer) == 0) remove (name.c_str());
 }
 
+void create_file(const std::string& filename, const std::string& contents);
+void create_file(const std::string& filename, const std::string& contents)
+{
+	remove_file_if_it_exists(filename);
+    std::ofstream file(filename);
+    file << contents;
+    file.close();
+}
+
+
 TEST_F(TextFileReaderTest, example)
 {
 //! [TextFileReaderTest example]
-	const std::string filename1 = "data_for_TextFileReaderTest_1";
-	const std::string filename2 = "data_for_TextFileReaderTest_2";
-	remove_file_if_it_exists(filename1);
-	remove_file_if_it_exists(filename2);
-    std::ofstream file1 (filename1);
+    const std::string filename1 = "data_for_TextFileReaderTest_1";
     const std::string contents_file1 = a.random<std::string>();
+    create_file(filename1, contents_file1);
+    const std::string filename2 = "data_for_TextFileReaderTest_2";
     const std::string contents_file2 = a.random<std::string>();
-    file1 << contents_file1;
-    file1.close();
-    std::ofstream file2 (filename2);
-    file2 << contents_file2;
-    file2.close();
+    create_file(filename2, contents_file2);
     std::vector<std::string> filenames;
     filenames.push_back(filename1);
     filenames.push_back(filename2);
@@ -68,18 +72,14 @@ TEST_F(TextFileReaderTest, example)
 TEST_F(TextFileReaderTest, should_represent_newlines_correctly)
 {
     const std::string filename = "data_for_TextFileReaderTest";
-    std::ofstream file (filename);
     const std::string contents_file = a.random<std::string>()()
                                              + std::string("\n")
                                      + a.random<std::string>()()
                                      + std::string("\n")
                                      + a.random<std::string>()();
-    file << contents_file;
-    file.close();
-    std::vector<std::string> filenames;
-    filenames.push_back(filename);
-    TextFileReader reader(filenames);
-    remove (filename.c_str());
+    create_file(filename, contents_file);
+    TextFileReader reader(std::vector<std::string>(1, filename));
+    remove_file_if_it_exists(filename);
     ASSERT_EQ(contents_file, reader.get_contents());
 }
 
