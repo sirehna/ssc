f35aa8aacf8e9a574f5f48b402d3aa8750c84599
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-03-02 14:19:46 +0100

    [doxygen] Fixed doxy example path for ssc/kinematics/KinematicTree.hpp

--- ssc/kinematics/KinematicTree.hpp
+++ ssc/kinematics/KinematicTree.hpp
@@ -27,9 +27,9 @@ namespace ssc
          *  \details Uses boost::graph's implementation of Dijkstra's algorithm. The tree is an undirected graph.
          *  \ingroup kinematics
          *  \section ex1 Example
-         *  \snippet kinematics/unit_tests/src/KinematicsTreeTest.cpp KinematicsTreeTest example
+         *  \snippet kinematics/unit_tests/src/KinematicTreeTest.cpp KinematicsTreeTest example
          *  \section ex2 Expected output
-         *  \snippet kinematics/unit_tests/src/KinematicsTreeTest.cpp KinematicsTreeTest expected output
+         *  \snippet kinematics/unit_tests/src/KinematicTreeTest.cpp KinematicsTreeTest expected output
          */
         class KinematicTree
         {
@@ -38,7 +38,7 @@ namespace ssc
                 /**  \author cec
                   *  \date 23 avr. 2014, 14:53:58
                   *  \brief Adds a connection between two (existing or non-existing) nodes in the kinematic tree
-                  *  \snippet kinematics/unit_tests/src/KinematicsTreeTest.cpp KinematicsTreeTest add_example
+                  *  \snippet kinematics/unit_tests/src/KinematicTreeTest.cpp KinematicsTreeTest add_example
                   */
                 void add(const std::string& frame_A, //!< First frame to connect
                          const std::string& frame_B  //!< Second frame to connect
