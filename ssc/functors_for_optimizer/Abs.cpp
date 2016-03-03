d169a18a57c550508689783bfb0ef95aec536803
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2016-03-03 14:52:59 +0100

    Added Abs & Sign
    Conflicts:
    	ssc/functors_for_optimizer/CMakeLists.txt
    	ssc/functors_for_optimizer/NodeVisitor.hpp
    	ssc/functors_for_optimizer/Serialize.cpp
    	ssc/functors_for_optimizer/Serialize.hpp
    	ssc/functors_for_optimizer/SerializeReversePolish.cpp
    	ssc/functors_for_optimizer/SerializeReversePolish.hpp
    	ssc/functors_for_optimizer/StateAndParameterCounter.cpp
    	ssc/functors_for_optimizer/StateGetter.cpp
    	ssc/functors_for_optimizer/StateGetter.hpp
    	ssc/functors_for_optimizer/unit_tests/CMakeLists.txt
    	ssc/jit_for_functors/JITVisitor.cpp
    	ssc/jit_for_functors/JITVisitor.hpp

---
+++ ssc/functors_for_optimizer/Abs.cpp
@@ -0,0 +1,63 @@
+/*
+ * Abs.cpp
+ *
+ * \date 1 f�vr. 2013, 14:40:27
+ *  \author cec
+ */
+
+#include "ssc/functors_for_optimizer/Abs.hpp"
+#include "ssc/functors_for_optimizer/State.hpp"
+#include "ssc/functors_for_optimizer/Constant.hpp"
+#include "ssc/functors_for_optimizer/Multiply.hpp"
+#include "ssc/functors_for_optimizer/Parameter.hpp"
+#include "ssc/functors_for_optimizer/FunctorAlgebra.hpp"
+#include "ssc/functors_for_optimizer/NodeVisitor.hpp"
+#include "ssc/functors_for_optimizer/Sign.hpp"
+#include <cmath>
+#include <string>
+
+using namespace ssc::functors_for_optimizer;
+
+Abs::Abs(const NodePtr& n_) : Unary(n_)
+{
+    update_lambda();
+}
+
+void Abs::update_lambda()
+{
+    const auto factor_ = factor;
+    const auto n_ = n;
+    set_value([factor_,n_]()->double {return factor_*std::abs(n_->get_lambda()());});
+}
+
+std::string Abs::get_operator_name() const
+{
+    return "abs";
+}
+
+NodePtr Abs::diff(const StatePtr& state) const
+{
+    auto du_dstate = n->diff(state);
+    auto dv_dstate = Sign(n->clone()).clone();
+    return du_dstate*dv_dstate;
+}
+
+NodePtr Abs::clone() const
+{
+    return AbsPtr(new Abs(*this));
+}
+
+bool Abs::is_null() const
+{
+    return get_son()->is_null();
+}
+
+std::string Abs::get_type() const
+{
+    return "Abs";
+}
+
+void Abs::accept(NodeVisitor& v) const
+{
+    v.visit(*this);
+}
---
+++ ssc/functors_for_optimizer/unit_tests/src/AbsTest.cpp
@@ -0,0 +1,74 @@
+/*
+ * AbsTest.cpp
+ *
+ *  Created on: Mar 3, 2016
+ *      Author: cady
+ */
+
+#include <cmath>
+#include "AbsTest.hpp"
+#include "ssc/functors_for_optimizer/Abs.hpp"
+
+using namespace ssc::functors_for_optimizer;
+
+#define X (*x)
+#define X_ (x->get_lambda()())
+
+AbsTest::AbsTest() : a(ssc::random_data_generator::DataGenerator(54515)), generate()
+{
+}
+
+AbsTest::~AbsTest()
+{
+}
+
+void AbsTest::SetUp()
+{
+}
+
+void AbsTest::TearDown()
+{
+}
+
+TEST_F(AbsTest, example)
+{
+//! [AbsTest example]
+    auto x = generate.state("x");
+    Abs s(x);
+//! [AbsTest example]
+//! [AbsTest expected output]
+    auto abs_X = s.get_lambda();
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        X = a.random<double>();
+        const double abs_x = std::abs((double)X_);
+        ASSERT_DOUBLE_EQ(abs_x, abs_X());
+    }
+//! [AbsTest expected output]
+}
+
+TEST_F(AbsTest, derivative)
+{
+    auto x = generate.state("x");
+    Abs s(x);
+    auto dabs_dx = s.diff(x)->get_lambda();
+
+    for (size_t i = 0 ; i < 1000 ; ++i)
+    {
+        X = a.random<double>();
+        const double sign_x = X_ >= 0 ? 1 : -1;
+        ASSERT_DOUBLE_EQ(sign_x, dabs_dx());
+    }
+}
+
+TEST_F(AbsTest, equality_operator)
+{
+    const auto x = generate.state("x");
+    const auto y = generate.state("y");
+    const Abs abs_x(x), abs_y(y);
+
+    ASSERT_TRUE(abs_x.equals(abs_x));
+    ASSERT_TRUE(abs_y.equals(abs_y));
+    ASSERT_FALSE(abs_x.equals(abs_y));
+    ASSERT_FALSE(abs_y.equals(abs_x));
+}
