55f2d67fed20ec89c9d5d079d398c9d1ea66f025
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2016-03-11 12:08:43 +0100

    Removed unused operators from FunctorAlgebra

--- ssc/functors_for_optimizer/FunctorAlgebra.hpp
+++ ssc/functors_for_optimizer/FunctorAlgebra.hpp
@@ -84,10 +84,6 @@ namespace ssc
         NodePtr operator/(const NodePtr& n1, const Node& n2);
         NodePtr operator/(const NodePtr& n, const double d);
         NodePtr operator/(const double d, const NodePtr& n);
-        NodePtr operator/(const NodePtr& n1, const Null& n2);
-        NodePtr operator/(const Null& n1, const NodePtr& n2);
-        NodePtr operator/(const Node& n1, const Null& n2);
-        NodePtr operator/(const Null& n1, const Node& n2);
 
         PowPtr pow(const Node& n1, const double d);
         PowPtr pow(const NodePtr& n1, const double d);
