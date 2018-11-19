0ea9a56a2e4d7663cc063fab76d591c7d3e7d2e1
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2018-11-19 14:34:42 +0100

    Added a few functions to ssc::json

---
+++ ssc/json/RapidJSON.cpp
@@ -0,0 +1,105 @@
+#include "RapidJSON.hpp"
+
+using namespace ssc::json;
+
+std::string ssc::json::print_type(const rapidjson::Value&  v)
+{
+    static const char* kTypeNames[] =
+          { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    return kTypeNames[v.GetType()];
+}
+
+double ssc::json::find_double(const std::string& key, const rapidjson::Value&  v)
+{
+    static const char* kTypeNames[] =
+          { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    rapidjson::Value::ConstMemberIterator itr = v.FindMember(key.c_str());
+    if (itr != v.MemberEnd())
+    {
+      if (itr->value.IsDouble() || itr->value.IsNumber())
+      {
+        return itr->value.GetDouble();
+      }
+      else
+      {
+        THROW(__PRETTY_FUNCTION__, JSONException, key << " should be a double, but it's a " << kTypeNames[itr->value.GetType()] << " in input JSON '" << dump(v) << "'");
+      }
+    }
+    else
+    {
+      THROW(__PRETTY_FUNCTION__, JSONException, "Could not find key '" << key << "' in input JSON '" << dump(v) << "'");
+    }
+    return 0;
+}
+
+int ssc::json::find_int(const std::string& key, const rapidjson::Value&  v)
+{
+    static const char* kTypeNames[] =
+          { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    rapidjson::Value::ConstMemberIterator itr = v.FindMember(key.c_str());
+    if (itr != v.MemberEnd())
+    {
+      if (itr->value.IsInt())
+      {
+        return itr->value.GetInt();
+      }
+      else
+      {
+        THROW(__PRETTY_FUNCTION__, JSONException, key << " should be a int, but it's a " << kTypeNames[itr->value.GetType()] << " in input JSON '" << dump(v) << "'");
+      }
+    }
+    else
+    {
+      THROW(__PRETTY_FUNCTION__, JSONException, "Could not find key '" << key << "' in input JSON '" << dump(v) << "'");
+    }
+    return 0;
+}
+
+std::string ssc::json::find_string(const std::string& key, const rapidjson::Value&  v)
+{
+    static const char* kTypeNames[] =
+          { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    rapidjson::Value::ConstMemberIterator itr = v.FindMember(key.c_str());
+    if (itr != v.MemberEnd())
+    {
+      if (itr->value.IsString())
+      {
+        return itr->value.GetString();
+      }
+      else
+      {
+        THROW(__PRETTY_FUNCTION__, JSONException, key << " should be a string, but it's a " << kTypeNames[itr->value.GetType()] << " in input JSON '" << dump(v) << "'");
+      }
+    }
+    else
+    {
+      THROW(__PRETTY_FUNCTION__, JSONException, "Could not find key '" << key << "' in input JSON '" << dump(v) << "'");
+    }
+    return "";
+}
+
+std::string ssc::json::dump(const rapidjson::Value& v)
+{
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    v.Accept(writer);
+    return buffer.GetString();
+}
+
+void ssc::json::parse(const std::string& json, rapidjson::Document& document)
+{
+    const rapidjson::ParseResult result = document.Parse(json.c_str());
+    if (!result)
+    {
+      THROW(__PRETTY_FUNCTION__, JSONException, "JSON parse error: " << GetParseError_En(result.Code()) << " (offset " << result.Offset() << ") in string " << json);
+    }
+}
+
+double ssc::json::find_optional_double(const std::string& key, const rapidjson::Value&  v, const double default_value)
+{
+    if (v.HasMember(key.c_str()))
+    {
+        return find_double(key, v);
+    }
+    return default_value;
+}
--- ssc/json/RapidJSON.hpp
+++ ssc/json/RapidJSON.hpp
@@ -4,45 +4,57 @@
 #include <iostream>
 
 #include "ssc/exception_handling/Exception.hpp"
-
-class JSONException: public ssc::exception_handling::Exception
-{
-    public:
-        InvalidInputException(const std::string& message, const std::string& file, const std::string& function, const unsigned int line) :
-               Exception(message, file, function, line)
-        {
-        }
-};
-
-#define RAPIDJSON_ASSERT(x) \
-   if (not(x))\
-   {\
-      if (strcmp("IsArray()", #x) == 0 )\
-      {\
-          throw JSONException("Expecting an array", __FILE__, __PRETTY_FUNCTION__, __LINE__);\
-      }\
-      else\
-      {\
-         throw JSONException(#x, __FILE__, __PRETTY_FUNCTION__, __LINE__);\
-      }\
-   }
-   //std::cout << "in file " << __FILE__ << ", line " << __LINE__ << ": " << #x << std::endl
-//#include <stdexcept>               // std::runtime_error
-//#include "rapidjson/error/error.h" // rapidjson::ParseResult
-/*struct ParseException : std::runtime_error, rapidjson::ParseResult
-{
-    ParseException(rapidjson::ParseErrorCode code, const char* msg, size_t offset) : std::runtime_error(msg), ParseResult(code, offset)
-    {}
-
-    virtual ~ParseException() {}
-};
-*/
-
 #include "rapidjson/document.h"
 #include "rapidjson/error/en.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 
+namespace ssc
+{
+    namespace json
+    {
+        class JSONException: public exception_handling::Exception
+        {
+            public:
+                JSONException(const std::string& message, const std::string& file, const std::string& function, const unsigned int line) :
+                       Exception(message, file, function, line)
+                {
+                }
+        };
+/*
+#define RAPIDJSON_ASSERT(x) \
+           if (not(x))\
+           {\
+              if (strcmp("IsArray()", #x) == 0 )\
+              {\
+                  throw JSONException("Expecting an array", __FILE__, __PRETTY_FUNCTION__, __LINE__);\
+              }\
+              else\
+              {\
+                 throw JSONException(#x, __FILE__, __PRETTY_FUNCTION__, __LINE__);\
+              }\
+           }
+           */
+           //std::cout << "in file " << __FILE__ << ", line " << __LINE__ << ": " << #x << std::endl
+        //#include <stdexcept>               // std::runtime_error
+        //#include "rapidjson/error/error.h" // rapidjson::ParseResult
+        /*struct ParseException : std::runtime_error, rapidjson::ParseResult
+        {
+            ParseException(rapidjson::ParseErrorCode code, const char* msg, size_t offset) : std::runtime_error(msg), ParseResult(code, offset)
+            {}
+
+            virtual ~ParseException() {}
+        };
+        */
+        std::string print_type(const rapidjson::Value&  v);
+        double find_double(const std::string& key, const rapidjson::Value&  v);
+        int find_int(const std::string& key, const rapidjson::Value&  v);
+        std::string find_string(const std::string& key, const rapidjson::Value&  v);
+        std::string dump(const rapidjson::Value& v);
+        void parse(const std::string& json, rapidjson::Document& document);
+        double find_optional_double(const std::string& key, const rapidjson::Value&  v, const double default_value);
+    }
+}
 
 
 #endif
