4515b1cb099faa30d268edc3420d25534c07b715
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-02-24 14:23:23 +0100

    :sparkles: Added a wuicksolve function with discrete systems

--- ssc/solver/Scheduler.hpp
+++ ssc/solver/Scheduler.hpp
@@ -19,106 +19,105 @@ namespace ssc
     {
         class Scheduler
         {
-            public:
-                typedef std::function<void(Scheduler& scheduler, ContinuousSystem* system)> Callback;
-                Scheduler(const double tstart, const double tend_, const double dt)
-                        : current_time(tstart)
-                        , scheduled_time_events()
-                        , discrete_state_updaters()
-                        , tend(tend_)
+          public:
+            typedef std::function<void(Scheduler &scheduler, ContinuousSystem *system)> Callback;
+            Scheduler(const double tstart, const double tend_, const double dt)
+                : current_time(tstart)
+                , scheduled_time_events()
+                , discrete_state_updaters()
+                , t0(tstart)
+                , tend(tend_)
+            {
+                if (dt > 0)
                 {
-                    if (dt > 0)
+                    double t = tstart;
+                    size_t i = 0;
+                    while (t + dt < tend)
                     {
-                        double t = tstart;
-                        size_t i = 0;
-                        while (t+dt < tend)
-                        {
-                            t = tstart + ((double)(++i))*dt;
-                            scheduled_time_events.push_back(t);
-                        }
-                        scheduled_time_events.push_back(tend);
-                        scheduled_time_events.sort();
-                    }
-                }
-
-                virtual ~Scheduler() = default;
-
-                virtual bool has_more_time_events() const
-                {
-                    return not(scheduled_time_events.empty());
-                }
-
-                /**
-                 * @brief Get the discrete state updaters to run & remove them from list.
-                 *
-                 * @return Callbacks to run to update the discrete states (e.g. one updater per controller).
-                 */
-                virtual std::vector<Callback> get_discrete_state_updaters_to_run()
-                {
-                    std::vector<Callback> updaters_to_run;
-                    std::vector<std::pair<double, Callback> > remaining_updaters;
-                    remaining_updaters.reserve(discrete_state_updaters.size());
-                    for (const auto t_updater:discrete_state_updaters)
-                    {
-                        if (t_updater.first <= current_time) // Need to run updater
-                        {
-                            updaters_to_run.push_back(t_updater.second);
-                        }
-                        else // Add updater to the list of updaters to keep
-                        {
-                            remaining_updaters.push_back(t_updater);
-                        }
-                    }
-                    // Only keep selected updaters, discard the rest
-                    discrete_state_updaters = remaining_updaters;
-                    return updaters_to_run;
-                }
-
-                virtual void add_time_event(const double t)
-                {
-                    if (t <=tend)
-                    {
+                        t = tstart + ((double)(++i)) * dt;
                         scheduled_time_events.push_back(t);
-                        scheduled_time_events.sort();
                     }
+                    scheduled_time_events.push_back(tend);
+                    scheduled_time_events.sort();
                 }
+            }
 
-                virtual void schedule_discrete_state_update(const double t, Callback& updater)
-                {
-                    discrete_state_updaters.push_back(std::make_pair(t, updater));
-                    add_time_event(t);
-                }
+            virtual ~Scheduler() = default;
 
-                virtual void advance_to_next_time_event()
+            virtual bool has_more_time_events() const { return not(scheduled_time_events.empty()); }
+
+            /**
+             * @brief Get the discrete state updaters to run & remove them from list.
+             *
+             * @return Callbacks to run to update the discrete states (e.g. one updater per
+             * controller).
+             */
+            virtual std::vector<Callback> get_discrete_state_updaters_to_run()
+            {
+                std::vector<Callback> updaters_to_run;
+                std::vector<std::pair<double, Callback> > remaining_updaters;
+                remaining_updaters.reserve(discrete_state_updaters.size());
+                for (const auto t_updater : discrete_state_updaters)
                 {
-                    if (not(scheduled_time_events.empty()))
+                    if (t_updater.first <= current_time) // Need to run updater
                     {
-                        current_time = scheduled_time_events.front();
-                        scheduled_time_events.pop_front();
+                        updaters_to_run.push_back(t_updater.second);
                     }
-                }
-
-                virtual double get_time() const
-                {
-                    return current_time;
-                }
-
-                virtual double get_step() const
-                {
-                    if (not(scheduled_time_events.empty()))
+                    else // Add updater to the list of updaters to keep
                     {
-                        const double next_time = scheduled_time_events.front();
-                        return next_time - current_time;
+                        remaining_updaters.push_back(t_updater);
                     }
-                    return 0;
                 }
+                // Only keep selected updaters, discard the rest
+                discrete_state_updaters = remaining_updaters;
+                return updaters_to_run;
+            }
 
-            private:
-                Scheduler();
-                double current_time;
-                std::list<double> scheduled_time_events;
-                std::vector<std::pair<double, Callback> > discrete_state_updaters;
-                double tend;
+            virtual void add_time_event(const double t)
+            {
+                if (t <= tend)
+                {
+                    scheduled_time_events.push_back(t);
+                    scheduled_time_events.sort();
+                }
+            }
+
+            virtual void schedule_discrete_state_update(const double t, Callback &updater)
+            {
+                discrete_state_updaters.push_back(std::make_pair(t, updater));
+                add_time_event(t);
+            }
+
+            virtual void advance_to_next_time_event()
+            {
+                if (not(scheduled_time_events.empty()))
+                {
+                    current_time = scheduled_time_events.front();
+                    scheduled_time_events.pop_front();
+                }
+            }
+
+            virtual double get_time() const { return current_time; }
+
+            virtual double get_step() const
+            {
+                if (not(scheduled_time_events.empty()))
+                {
+                    const double next_time = scheduled_time_events.front();
+                    return next_time - current_time;
+                }
+                return 0;
+            }
+
+            double get_t0() const { return t0; }
+
+          private:
+            Scheduler();
+            double current_time;
+            std::list<double> scheduled_time_events;
+            std::vector<std::pair<double, Callback> > discrete_state_updaters;
+            double t0;
+            double tend;
         };
     }
 }
--- ssc/solver/solve.hpp
+++ ssc/solver/solve.hpp
@@ -8,24 +8,27 @@
 #ifndef SOLVE_HPP_
 #define SOLVE_HPP_
 
+#include "ssc/solver/DiscreteSystem.hpp"
 #include "ssc/solver/EventHandler.hpp"
-#include "ssc/solver/Scheduler.hpp"
 #include "ssc/solver/Observers.hpp"
+#include "ssc/solver/Scheduler.hpp"
 
+#include "ssc/macros/tr1_macros.hpp"
 #include <boost/numeric/odeint/stepper/controlled_step_result.hpp>
+
+#include TR1INC(memory)
 
 namespace ssc
 {
     namespace solver
     {
-        template <typename StepperType,
-                  typename SystemType>
-        void
-        solve_for_constant_step(SystemType& sys, Observer& observer, StepperType& stepper, Scheduler& scheduler, EventHandler& event_handler)
+        template <typename StepperType, typename SystemType>
+        void solve_for_constant_step(SystemType &sys, Observer &observer, StepperType &stepper,
+                                     Scheduler &scheduler, EventHandler &event_handler)
         {
             const double tstart = scheduler.get_time();
-            observer.observe(sys,tstart);
-            while(scheduler.has_more_time_events())
+            observer.observe(sys, tstart);
+            while (scheduler.has_more_time_events())
             {
                 const double dt = scheduler.get_step();
                 const double t = scheduler.get_time();
@@ -36,7 +39,7 @@ namespace ssc
                     event_handler.run_event_actions();
                 }
                 const auto discrete_state_updaters = scheduler.get_discrete_state_updaters_to_run();
-                for (const auto state_updater:discrete_state_updaters)
+                for (const auto state_updater : discrete_state_updaters)
                 {
                     state_updater(scheduler, &sys);
                 }
@@ -45,18 +48,18 @@ namespace ssc
             }
         }
 
-        template <typename StepperType,
-                  typename SystemType>
-        void
-        solve_for_adaptive_step(SystemType& sys, Observer& observer, StepperType& stepper, Scheduler& scheduler, EventHandler& event_handler)
+        template <typename StepperType, typename SystemType>
+        void solve_for_adaptive_step(SystemType &sys, Observer &observer, StepperType &stepper,
+                                     Scheduler &scheduler, EventHandler &event_handler)
         {
             const double tstart = scheduler.get_time();
-            observer.observe(sys,tstart);
-            while(scheduler.has_more_time_events())
+            observer.observe(sys, tstart);
+            while (scheduler.has_more_time_events())
             {
                 const double t = scheduler.get_time();
                 const double dt = scheduler.get_step();
-                const boost::numeric::odeint::controlled_step_result res = stepper.try_step(sys, sys.state, t, dt);
+                const boost::numeric::odeint::controlled_step_result res
+                    = stepper.try_step(sys, sys.state, t, dt);
                 if (res == boost::numeric::odeint::success)
                 {
                     if (event_handler.detected_state_events())
@@ -64,8 +67,9 @@ namespace ssc
                         event_handler.locate_event();
                         event_handler.run_event_actions();
                     }
-                    const auto discrete_state_updaters = scheduler.get_discrete_state_updaters_to_run();
-                    for (const auto state_updater:discrete_state_updaters)
+                    const auto discrete_state_updaters
+                        = scheduler.get_discrete_state_updaters_to_run();
+                    for (const auto state_updater : discrete_state_updaters)
                     {
                         state_updater(scheduler, &sys);
                     }
@@ -74,18 +78,32 @@ namespace ssc
                 }
                 else
                 {
-                    scheduler.add_time_event(t + dt/2);
+                    scheduler.add_time_event(t + dt / 2);
                     scheduler.advance_to_next_time_event();
                 }
             }
         }
-        template <typename StepperType,
-                  typename SystemType>
-        void quicksolve(SystemType& sys, Scheduler& scheduler, Observer& observer)
+        template <typename StepperType, typename SystemType>
+        void quicksolve(SystemType &sys, Scheduler &scheduler, Observer &observer)
         {
             StepperType stepper;
             EventHandler event_handler;
-            solve_for_constant_step<StepperType,SystemType>(sys, observer, stepper, scheduler, event_handler);
+            solve_for_constant_step<StepperType, SystemType>(sys, observer, stepper, scheduler,
+                                                             event_handler);
+        }
+        typedef TR1(shared_ptr)<DiscreteSystem> DiscreteSystemPtr;
+        template <typename StepperType, typename SystemType>
+        void quicksolve(SystemType &sys, std::vector<DiscreteSystemPtr> &discrete_systems,
+                        Scheduler &scheduler, Observer &observer)
+        {
+            StepperType stepper;
+            EventHandler event_handler;
+            for (const auto &discrete_system : discrete_systems)
+            {
+                discrete_system->schedule_update(scheduler.get_t0(), scheduler);
+            }
+            solve_for_constant_step<StepperType, SystemType>(sys, observer, stepper, scheduler,
+                                                             event_handler);
         }
     }
 }
