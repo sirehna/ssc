c9b88e683a72637c73987862d1d001f230482b5f
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-02-24 11:11:40 +0100

    :white_check_mark: Added a test with RK4
    Just to make sure discrete states behave the same way for steppers that
    call the continuous model several times.

--- ssc/solver/unit_tests/src/DiscreteSystemsTests.cpp
+++ ssc/solver/unit_tests/src/DiscreteSystemsTests.cpp
@@ -15,6 +15,7 @@
 #include "test_systems.hpp"
 
 #include <boost/numeric/odeint/stepper/euler.hpp>
+#include <boost/numeric/odeint/stepper/runge_kutta4.hpp>
 
 #include <sstream>
 
@@ -35,6 +36,7 @@ void DiscreteSystemsTests::TearDown()
 }
 
 typedef boost::numeric::odeint::euler<StateType> EulerStepper;
+typedef boost::numeric::odeint::runge_kutta4<StateType> RungeKuttaStepper;
 
 struct ContinuousSystem : public ssc::solver::ContinuousSystem
 {
@@ -93,6 +95,33 @@ TEST_F(DiscreteSystemsTests, quarter_second_steps)
     DiscreteSystem discrete_system(0.3);
     discrete_system.schedule_update(7.325, scheduler);
     ssc::solver::quicksolve<EulerStepper>(continuous_system, scheduler, observer);
+    const auto observations = observer.get();
+    ASSERT_EQ(22, discrete_system.ticks.back());
+    // Check instants
+    const std::vector<double> expected_observation_instants = {6, 6.2, 6.4, 6.6, 6.8, 7, 7.2, 7.325, 7.4, 7.6, 7.625, 7.8, 7.925, 8, 8.2, 8.225, 8.4, 8.525, 8.6, 8.8, 8.825, 9, 9.125, 9.2, 9.4, 9.425, 9.6, 9.725, 9.8, 10};
+    ASSERT_EQ(expected_observation_instants.size(), observations.size());
+    for (size_t i = 0 ; i < 30 ; ++i)
+    {
+        ASSERT_DOUBLE_EQ(expected_observation_instants.at(i), observations.at(i).first);
+    }
+    // Check values
+    for (const auto& observation:observations)
+    {
+        ASSERT_DOUBLE_EQ(observation.first - t0, observation.second);
+    }
+}
+
+TEST_F(DiscreteSystemsTests, quarter_second_steps_with_rk4)
+{
+    ssc::solver::VectorObserver observer;
+    const double t0 = 6;
+    const double tend = 10;
+    const double dt = 0.2;
+    ssc::solver::Scheduler scheduler(t0, tend, dt);
+    ContinuousSystem continuous_system(std::vector<double>(1,0));
+    DiscreteSystem discrete_system(0.3);
+    discrete_system.schedule_update(7.325, scheduler);
+    ssc::solver::quicksolve<RungeKuttaStepper>(continuous_system, scheduler, observer);
     const auto observations = observer.get();
     ASSERT_EQ(22, discrete_system.ticks.back());
     // Check instants
